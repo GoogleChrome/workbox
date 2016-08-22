abb26ecac046f8491a958b075a86b3c219b193e2
Philip Walton <philipwalton@users.noreply.github.com>
2016-08-22 13:42:00 -0700

    Add hitFilter option and better support module loaders (#39)
    * Add support for requiring via npm module loaders
    
    * Extend the retry amount time
    
    * Add logic to replay hits more proactively
    
    * Add default function arguments
    
    Also upgraded eslint due to bugs with default function argument parsing in the
    previous version.
    
    * Return an error Response instead of an Error
    
    * Add idb to dependencies
    
    * Add support for a hitFilter config option
    
    * Update jsdoc comments and fix lint errors
    
    * Address review comments

--- projects/sw-offline-google-analytics/src/lib/replay-queued-requests.js
+++ projects/sw-offline-google-analytics/src/lib/replay-queued-requests.js
@@ -15,7 +15,6 @@
 
 const IDBHelper = require('../../../../lib/idb-helper.js');
 const constants = require('./constants.js');
-const log = require('../../../../lib/log.js');
 
 const idbHelper = new IDBHelper(constants.IDB.NAME, constants.IDB.VERSION,
   constants.IDB.STORE);
@@ -27,14 +26,23 @@ const idbHelper = new IDBHelper(constants.IDB.NAME, constants.IDB.VERSION,
  * Returns a promise that resolves when the replaying is complete.
  *
  * @private
- * @param {Object=} additionalParameters URL parameters, expressed as key/value
- *                 pairs, to be added to replayed Google Analytics requests.
- *                 This can be used to, e.g., set a custom dimension indicating
- *                 that the request was replayed from the service worker.
+ * @param {Object=}   config Optional configuration arguments.
+ * @param {Object=}   config.parameterOverrides Optional
+ *                    [Measurement Protocol parameters](https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters),
+ *                    expressed as key/value pairs, to be added to replayed
+ *                    Google Analytics requests. This can be used to, e.g., set
+ *                    a custom dimension indicating that the request was
+ *                    replayed.
+ * @param {Function=} config.hitFilter Optional
+ *                    A function that allows you to modify the hit parameters
+ *                    prior to replaying the hit. The function is invoked with
+ *                    the original hit's URLSearchParams object as its only
+ *                    argument. To abort the hit and prevent it from being
+ *                    replayed, throw an error.
  * @returns {Promise.<T>}
  */
-module.exports = additionalParameters => {
-  additionalParameters = additionalParameters || {};
+module.exports = (config) => {
+  config = config || {};
 
   return idbHelper.getAllKeys().then(urls => {
     return Promise.all(urls.map(url => {
@@ -42,31 +50,42 @@ module.exports = additionalParameters => {
         const queueTime = Date.now() - hitTime;
         const newUrl = new URL(url);
 
-        // URLSearchParams was added in Chrome 49.
-        // On the off chance we're on a browser that lacks support, we won't
-        // set additionParameters, but at least we'll set qt=.
-        if ('searchParams' in newUrl) {
-          additionalParameters.qt = queueTime;
-          // Call sort() on the keys so that there's a reliable order of calls
-          // to searchParams.set(). This isn't important in terms of
-          // functionality, but it will make testing easier, since the
-          // URL serialization depends on the order in which .set() is called.
-          Object.keys(additionalParameters).sort().forEach(parameter => {
-            newUrl.searchParams.set(parameter, additionalParameters[parameter]);
-          });
-        } else {
-          log('The browser does not support URLSearchParams, ' +
-            'so not setting additional parameters.');
-          newUrl.search += (newUrl.search ? '&' : '') + 'qt=' + queueTime;
+        // Do not attempt to replay hits that are too old.
+        if (queueTime > constants.STOP_RETRYING_AFTER) {
+          return;
         }
 
-        return fetch(newUrl.toString()).catch(error => {
-          // If this was queued recently, then rethrow the error, to prevent
-          // the entry from being deleted. It will be retried again later.
-          if ((Date.now() - queueTime) < constants.STOP_RETRYING_AFTER) {
-            throw error;
-          }
+        // Do not attempt to replay hits in browsers without
+        // URLSearchParams support.
+        if (!('searchParams' in newUrl)) {
+          return;
+        }
+
+        let parameterOverrides = config.parameterOverrides || {};
+        parameterOverrides.qt = queueTime;
+
+        // Call sort() on the keys so that there's a reliable order of calls
+        // to searchParams.set(). This isn't important in terms of
+        // functionality, but it will make testing easier, since the
+        // URL serialization depends on the order in which .set() is called.
+        Object.keys(parameterOverrides).sort().forEach(parameter => {
+          newUrl.searchParams.set(parameter, parameterOverrides[parameter]);
         });
+
+        // If the hitFilter config option was passed and is a function,
+        // invoke it with searchParams as its argument allowing the function
+        // to modify the hit prior to sending it. The function can also
+        // throw an error to abort the hit if needed.
+        let hitFilter = config.hitFilter;
+        if (typeof hitFilter === 'function') {
+          try {
+            hitFilter(newUrl.searchParams);
+          } catch (err) {
+            return;
+          }
+        }
+
+        return fetch(newUrl.toString());
       }).then(() => idbHelper.delete(url));
     }));
   });
--- projects/sw-offline-google-analytics/test/unit/replay-queued-requests.js
+++ projects/sw-offline-google-analytics/test/unit/replay-queued-requests.js
@@ -26,15 +26,14 @@ const replayRequests = require('../../src/lib/replay-queued-requests.js');
 const idbHelper = new IDBHelper(constants.IDB.NAME, constants.IDB.VERSION,
   constants.IDB.STORE);
 
-const testLogic = (initialUrls, expectedUrls, time, additionalParameters) => {
+const testLogic = (initialUrls, expectedUrls, time, opts) => {
   return Promise.all(initialUrls.map(url => {
     return enqueueRequest(new Request(url), time);
-  })).then(() => replayRequests(additionalParameters))
+  })).then(() => replayRequests(opts))
     .then(() => fetchMock.calls().matched.map(match => match[0]))
-    .then(matchedUrls =>
-      chai.expect(matchedUrls).to.include.members(expectedUrls))
+    .then(matchedUrls => chai.expect(matchedUrls).to.deep.equal(expectedUrls))
     .then(() => idbHelper.getAllKeys())
-    .then(keys => chai.expect(keys).to.not.include.members(expectedUrls));
+    .then(keys => chai.expect(keys.length).to.equal(0));
 };
 
 describe('replay-queued-requests', () => {
@@ -42,12 +41,13 @@ describe('replay-queued-requests', () => {
   const initialTimestamp = 1470405670000; // An arbitrary, but valid, timestamp in milliseconds.
   const timestampOffset = 1000; // A 1000 millisecond offset.
 
-  before(() => {
+  beforeEach(() => {
     fetchMock.mock(`^${urlPrefix}`, new Response());
     MockDate.set(initialTimestamp + timestampOffset);
   });
 
-  after(() => {
+  afterEach(() => {
+    fetchMock.reset();
     MockDate.reset();
   });
 
@@ -63,24 +63,63 @@ describe('replay-queued-requests', () => {
     return testLogic(urls, urlsWithQt, time);
   });
 
-  it('should replay queued requests with additional parameters', () => {
+  it('should replay queued requests with parameters overrides', () => {
     const urls = ['one', 'two?three=4'].map(suffix => urlPrefix + suffix);
     const time = initialTimestamp;
-    const additionalParameters = {
+    const parameterOverrides = {
       three: 5,
       four: 'six',
       qt: timestampOffset
     };
-    const urlsWithAdditionalParameters = urls.map(url => {
+    const urlsWithParameterOverrides = urls.map(url => {
       const newUrl = new URL(url);
-      Object.keys(additionalParameters).sort().forEach(parameter => {
-        newUrl.searchParams.set(parameter, additionalParameters[parameter]);
+      Object.keys(parameterOverrides).sort().forEach(parameter => {
+        newUrl.searchParams.set(parameter, parameterOverrides[parameter]);
       });
       return newUrl.toString();
     });
 
-    return testLogic(urls, urlsWithAdditionalParameters, time,
-      additionalParameters);
+    return testLogic(urls, urlsWithParameterOverrides, time,
+      {parameterOverrides});
+  });
+
+  it('should replay queued requests with a hit filter', () => {
+    const urls = ['one', 'two?three=4'].map(suffix => urlPrefix + suffix);
+    const time = initialTimestamp;
+    const hitFilter = searchParams => {
+      const qt = searchParams.get('qt');
+      searchParams.set('cm1', qt);
+    };
+    const urlsWithHitFilterApplied = urls.map(url => {
+      const newUrl = new URL(url);
+      newUrl.searchParams.set('qt', timestampOffset);
+      newUrl.searchParams.set('cm1', timestampOffset);
+      return newUrl.toString();
+    });
+
+    return testLogic(urls, urlsWithHitFilterApplied, time,
+      {hitFilter});
+  });
+
+  it('should not a replay queued requests when hit filter throws', () => {
+    const urls = ['one', 'two?three=4'].map(suffix => urlPrefix + suffix);
+    const time = initialTimestamp;
+    const hitFilter = searchParams => {
+      const qt = searchParams.get('qt');
+      searchParams.set('cm1', qt);
+      if (searchParams.get('three') === '4') {
+        throw new Error('abort!');
+      }
+    };
+    const urlsWithHitFilterApplied = urls.slice(0, 1).map(url => {
+      const newUrl = new URL(url);
+      newUrl.searchParams.set('qt', timestampOffset);
+      newUrl.searchParams.set('cm1', timestampOffset);
+      return newUrl.toString();
+    });
+
+    return testLogic(urls, urlsWithHitFilterApplied, time,
+      {hitFilter});
   });
 
   after(() => {
