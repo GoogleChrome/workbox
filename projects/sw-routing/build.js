ef759feabcc01c7cc31808a1ad81199d022ae684
Matt Gaunt <matt@gauntface.co.uk>
2016-10-19 16:17:36 -0700

    First attempt and minified build

--- projects/sw-routing/build.js
+++ projects/sw-routing/build.js
@@ -15,26 +15,56 @@
 
 const path = require('path');
 const rollup = require('rollup').rollup;
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
 const pkg = require('./package.json');
 
 const destPath = path.join(__dirname, 'build');
 
 // This defines our the src is transpiled (i.e. one for ES Modules, one for UMD)
-const buildTargets = [{
+const umdBuildTargets = {
   dest: path.join(destPath, pkg.main),
   format: 'umd',
   moduleName: 'goog.routing',
   sourceMap: true
-}, {
+};
+
+const esNextBuildTarget = {
   dest: path.join(destPath, pkg['jsnext:main']),
   format: 'es',
   sourceMap: true
-}];
+};
 
-module.exports = () => {
+const minifiedUMD = () => {
+  return rollup({
+    entry: path.join(__dirname, 'src', 'index.js'),
+    plugins: [
+      babel({
+        presets: [
+          [
+            'es2015',
+            {modules: false}
+          ]
+        ],
+        plugins: ['external-helpers']
+      }),
+      uglify()
+    ]
+  })
+  .then(bundle => {
+    return bundle.write(umdBuildTargets);
+  });
+};
+
+const bundledJSNext = () => {
   return rollup({
     entry: path.join(__dirname, 'src', 'index.js')
-  }).then(bundle => Promise.all(
-    buildTargets.map(target => bundle.write(target))
-  ));
+  })
+  .then(bundle => {
+    return bundle.write(esNextBuildTarget);
+  });
+};
+
+module.exports = () => {
+  return Promise.all([minifiedUMD(), bundledJSNext()]);
 };
