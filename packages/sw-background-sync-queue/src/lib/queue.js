340456f4ecfe338bf9afe2559af2b1acc9d2a47a
Prateek Bhatnagar <prateek89born@gmail.com>
2016-11-15 19:51:13 +0530

    core Queue class jsDocs

--- packages/sw-background-sync-queue/src/lib/queue.js
+++ packages/sw-background-sync-queue/src/lib/queue.js
@@ -6,16 +6,42 @@ import bcmanager from './broadcastManager';
 let _queue;
 let _config;
 
+/**
+ * Core queue class that handles all the enqueue and dequeue
+ * as well as cleanup code for the background sync queue
+ * @class Queue
+ */
 class Queue {
+	/**
+	 * Creates an instance of Queue.
+	 *
+	 * @memberOf Queue
+	 */
 	constructor() {
 		this.initialize();
 	}
 
+	/**
+	 * initialize the queue object from the idb
+	 * giving access to any penging queues
+	 * @param {Object} config config that gives values like maxAge to specify the
+	 *
+	 * @memberOf Queue
+	 */
 	async initialize(config) {
 		_config = config;
 		_queue = await idbQHelper.get('queue') || [];
 	}
 
+	/**
+	 * push any request to background sync queue which would be played later
+	 * prefferably when network comes back
+	 *
+	 * @param {Request} request request object to be queued by this
+	 * @param {Object} config optional config to override config params
+	 *
+	 * @memberOf Queue
+	 */
 	async push(request, config) {
 		let localConfig = Object.assign({}, _config, config);
 		let hash = stringHash(request.url + JSON.stringify(request.headers)
@@ -47,6 +73,12 @@ class Queue {
 		}
 	}
 
+	/**
+	 * clean up the queue, deleting all the tasks who are either damaged or
+	 * whose maxAge has expired
+	 *
+	 * @memberOf Queue
+	 */
 	async cleanupQueue() {
 		let currIndex = 0;
 		for(const hash of _queue) {
@@ -64,6 +96,14 @@ class Queue {
 		idbQHelper.put('queue', _queue);
 	}
 
+	/**
+	 * get the Request from the queue at a particular index
+	 *
+	 * @param {int} index
+	 * @return {Request} hash of the request at the given index
+	 *
+	 * @memberOf Queue
+	 */
 	async getRequestFromQueueAtIndex( index ) {
 		if(!_queue[index]) {
 			return;
@@ -73,10 +113,25 @@ class Queue {
 		return reqData;
 	}
 
+	/**
+	 * get the hash from the queue at a particular index
+	 *
+	 * @param {int} index
+	 * @return {string} hash of the request at the given index
+	 *
+	 * @memberOf Queue
+	 */
 	getHash( index ) {
 		return _queue[index];
 	}
 
+	/**
+	 * get the total no of tasks in the queue
+	 *
+	 * @return {int} total number of tasks
+	 *
+	 * @memberOf Queue
+	 */
 	getTotalTasks() {
 		return _queue.length;
 	}
