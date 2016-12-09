852bf58dfc0ceae50f5a4135b683fb41e136afc7
Matt Gaunt <matt@gauntface.co.uk>
2016-12-08 19:23:01 -0800

    precaching-rework

--- packages/sw-precaching/test/browser-unit/data/test-data.js
+++ packages/sw-precaching/test/browser-unit/data/test-data.js
@@ -24,14 +24,24 @@ const addNewEntry = (origin) => {
     fileIndex++;
 
     EXAMPLE_REVISIONED_FILES_SET_1_STEP_1.push({
-      path: `${origin}${echoPath}/${fileIndex}.txt`,
+      request: `${origin}${echoPath}/${fileIndex}.txt`,
       revision: revision1[i],
     });
     EXAMPLE_REVISIONED_FILES_SET_1_STEP_2.push({
-      path: `${origin}${echoPath}/${fileIndex}.txt`,
+      request: `${origin}${echoPath}/${fileIndex}.txt`,
       revision: revision2[i],
     });
     fileIndex++;
+
+    /** EXAMPLE_REVISIONED_FILES_SET_1_STEP_1.push({
+      path: new Request(`${origin}${echoPath}/${fileIndex}.${revision1[i]}.txt`),
+      revision: revision1[i],
+    });
+    EXAMPLE_REVISIONED_FILES_SET_1_STEP_2.push({
+      path: new Request(`${origin}${echoPath}/${fileIndex}.${revision2[i]}.txt`),
+      revision: revision2[i],
+    });
+    fileIndex++;**/
   }
 };
 
