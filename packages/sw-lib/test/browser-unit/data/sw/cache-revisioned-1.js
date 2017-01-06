1787435539a06dfca20188d78064140735193a3a
Matt Gaunt <matt@gauntface.co.uk>
2017-01-05 17:09:45 -0800

    hooking up warm runtime to swlib

--- packages/sw-lib/test/browser-unit/data/sw/cache-revisioned-1.js
+++ packages/sw-lib/test/browser-unit/data/sw/cache-revisioned-1.js
@@ -5,11 +5,12 @@ importScripts('/packages/sw-precaching/test/browser-unit/data/skip-and-claim.js'
 importScripts('/packages/sw-precaching/test/browser-unit/data/test-data.js');
 importScripts('/packages/sw-lib/test/browser-unit/data/test-data.js');
 
-goog.swlib.cacheRevisionedAssets(self.goog.__TEST_DATA['sw-lib']['set-1']);
+const testSet = self.goog.__TEST_DATA['sw-lib']['revisioned'];
+goog.swlib.cacheRevisionedAssets(testSet['set-1']);
 
-goog.swlib.cacheRevisionedAssets(self.goog.__TEST_DATA['sw-lib']['set-2']);
+goog.swlib.cacheRevisionedAssets(testSet['set-2']);
 
-goog.swlib.cacheRevisionedAssets(self.goog.__TEST_DATA['sw-lib']['set-3']);
+goog.swlib.cacheRevisionedAssets(testSet['set-3']);
 
 // This allows tests to query the response from the SW.
 self.addEventListener('fetch', (event) => {
