1c461aa3fbd16f2d7df8493e030409dae0831fba
Matt Gaunt <matt@gauntface.co.uk>
2017-02-16 16:26:25 -0800

    Updates dependencies

--- packages/sw-lib/test/automated-test-suite.js
+++ packages/sw-lib/test/automated-test-suite.js
@@ -18,7 +18,6 @@ const swTestingHelpers = require('sw-testing-helpers');
 const testServer = require('../../../utils/test-server.js');
 
 require('chromedriver');
-require('operadriver');
 require('geckodriver');
 
 const RETRIES = 3;
@@ -86,12 +85,6 @@ describe('sw-lib Tests', function() {
     switch(browser.getId()) {
       case 'chrome':
       case 'firefox':
-      case 'opera':
-        if (browser.getId() === 'opera' &&
-          browser.getVersionNumber() <= 43) {
-          console.log(`Skipping Opera <= 43 due to driver issues.`);
-          return;
-        }
         setupTestSuite(browser);
         break;
       default:
