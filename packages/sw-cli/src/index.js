7c40013f8640fda6289baeefe5856ca1ec00ddd1
Matt Gaunt <matt@gauntface.co.uk>
2017-02-21 11:06:23 -0800

    Removing large comment

--- packages/sw-cli/src/index.js
+++ packages/sw-cli/src/index.js
@@ -167,39 +167,6 @@ class SWCli {
       });
     });
   }
-
-  /**
-   * @private
-   * @return {Promise<String>}
-   */
-  /** _getFileManifestName() {
-    return inquirer.prompt([
-      {
-        name: 'fileManifestName',
-        message: 'What should we name the file manifest?',
-        type: 'input',
-        default: 'precache-manifest.js',
-      },
-    ])
-    .then((results) => {
-      const manifestName = results.fileManifestName.trim();
-      if (manifestName.length === 0) {
-        logHelper.error(
-          errors['invalid-file-manifest-name']
-        );
-        throw new Error(errors['invalid-file-manifest-name']);
-      }
-
-      return manifestName;
-    })
-    .catch((err) => {
-      logHelper.error(
-        errors['unable-to-get-file-manifest-name'],
-        err
-      );
-      throw err;
-    });
-  }**/
 }
 
 module.exports = SWCli;
