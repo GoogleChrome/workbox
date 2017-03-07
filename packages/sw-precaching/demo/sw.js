5a3322863d92f65ecce9eba8e1dd177596612bac
Matt Gaunt <matt@gauntface.co.uk>
2017-03-06 17:18:39 -0800

    Adding a demo

---
+++ packages/sw-precaching/demo/sw.js
@@ -0,0 +1,42 @@
+importScripts('./sw-precaching.v0.0.13.min.js');
+importScripts('./manifest.123456.js');
+
+const revCacheManager = new goog.precaching.RevisionedCacheManager();
+revCacheManager.addToCacheList(self.__file_manifest);
+
+const unrevCacheManager = new goog.precaching.UnrevisionedCacheManager();
+unrevCacheManager.addToCacheList({
+  unrevisionedFiles: [
+    '/',
+    '/images/logo.png',
+  ],
+});
+
+self.addEventListener('install', (event) => {
+  const promiseChain = Promise.all([
+    revCacheManager.install(),
+    unrevCacheManager.install(),
+  ]);
+  event.waitUntil(promiseChain);
+});
+
+self.addEventListener('activate', (event) => {
+  const promiseChain = Promise.all([
+    revCacheManager.cleanup(),
+    unrevCacheManager.cleanup(),
+  ]);
+  event.waitUntil(promiseChain);
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+    .then((match) => {
+      if (match) {
+        return match;
+      }
+
+      return fetch(event.request);
+    })
+  );
+});
