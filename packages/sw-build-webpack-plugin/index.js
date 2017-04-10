180b8ef703ba95778552410bf9b464a8b8cec429
Prateek Bhatnagar <prateek89born@gmail.com>
2017-04-10 21:22:13 +0530

    adding inject manifest file functionality

--- packages/sw-build-webpack-plugin/index.js
+++ packages/sw-build-webpack-plugin/index.js
@@ -30,8 +30,27 @@ class SwBuildWebpackPlugin {
 	 * @memberOf SwBuildWebpackPlugin
 	 */
 	constructor(config) {
-		this._config = config;
+		this._config = config || {};
 	}
+	getConfig(compilation) {
+		let config = this._config;
+
+		// If no root directory is given, fallback to
+		// output path directory of webpack
+		if (!config.rootDirectory) {
+			config.rootDirectory = compilation.mainTemplate.getPublicPath({});
+		}
+
+		// If no dest is given, fallback to default service worker name in
+		// output path directory
+		if (!config.dest) {
+			config.dest =
+				path.join(config.rootDirectory, SERVICE_WORKER_NAME);
+		}
+
+		return config;
+	}
+
 	/**
 	 *
 	 * @param {Object} [compiler] default compiler object passed from webpack
@@ -40,20 +59,17 @@ class SwBuildWebpackPlugin {
 	 */
 	apply(compiler) {
 		compiler.plugin('after-emit', (compilation, callback) => {
-			// If no root directory is given, fallback to
-			// output path directory of webpack
-			if (!this._config.rootDirectory) {
-				this._config.rootDirectory = compilation.mainTemplate.getPublicPath({});
-			}
-			// If no dest is given, fallback to default service worker name in
-			// output path directory
-			if (!this._config.dest) {
-				this._config.dest =
-					path.join(this._config.rootDirectory, SERVICE_WORKER_NAME);
-			}
-			swBuild.generateSW(this._config)
+			const config = this.getConfig(compilation);
+
+			if (config.swFile) {
+				swBuild.injectManifest(config)
+					.then(() => callback())
+					.catch((e) => callback(e));
+			} else {
+				swBuild.generateSW(config)
 				.then(() => callback())
 				.catch((e) => callback(e));
+			}
 		});
 	}
 }
