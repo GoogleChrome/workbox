75bf4e6a7295885bcafa71b9d8a51a1b033f56a3
Matt Gaunt <matt@gauntface.co.uk>
2017-04-17 20:50:42 -0700

    Fixing up the error message in sw-cli. Fixes #396 (#433)
    * Fixing up the error message in sw-cli. Fixes #396
    
    * Log errors in ask question
    
    * Fixing up some of the errors in sw-cli

--- packages/sw-build/src/lib/errors.js
+++ packages/sw-build/src/lib/errors.js
@@ -1,13 +1,7 @@
 module.exports = {
   'unable-to-get-rootdir': 'Unable to get the root directory of your web app.',
-  'unable-to-get-file-extensions': 'Unable to get file extensions to ' +
-    'determine files to cache.',
   'no-extension': 'Unable to detect a usable extension for a file in your ' +
     'web app directory.',
-  'no-file-extensions-found': 'No files could be found that are suitable for ' +
-    'caching.',
-  'no-file-extensions-selected': 'No file extensions were selected so nothing' +
-    ' would be cached.',
   'invalid-file-manifest-name': 'The File Manifest Name must have at lease ' +
     'one character.',
   'unable-to-get-file-manifest-name': 'Unable to get a file manifest name.',
