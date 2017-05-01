d89533c384fd3127796aee11b96fcece68d53185
Matt Gaunt <matt@gauntface.co.uk>
2017-05-01 13:14:59 -0700

    Slight tweak to the comment

--- packages/sw-build/src/lib/generate-sw.js
+++ packages/sw-build/src/lib/generate-sw.js
@@ -53,7 +53,7 @@ const errors = require('./errors');
  * @param {string} [input.directoryIndex] An optional string that will
  * append this string to urls ending with '/' (Defaults to 'index.html').
  * @param {string} [input.navigateFallback] An optional string that will
- * attempt to serve this URL from cache.
+ * attempt to serve the response for the URL defined as this option from cache.
  * @return {Promise} Resolves once the service worker has been generated
  * with a precache list.
  *
