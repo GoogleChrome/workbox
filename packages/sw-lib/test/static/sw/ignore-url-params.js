f2b16c80bc7f3cb4de9637e192635839f9fbc346
Matt Gaunt <matt@gauntface.co.uk>
2017-05-03 13:34:13 -0700

    Adding ignoreUrlParamsMatching to sw-lib

---
+++ packages/sw-lib/test/static/sw/ignore-url-params.js
@@ -0,0 +1,15 @@
+/* global goog */
+
+importScripts('/packages/sw-lib/build/sw-lib.min.js');
+importScripts('/packages/sw-precaching/test/static/skip-and-claim.js');
+
+const swlib = new goog.SWLib();
+swlib.precache([
+  '/__echo/date/hello',
+]);
+
+// sw-lib should define a route just for the revisioned assets so this
+// fetch should never be called.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(null);
+});
