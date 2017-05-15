649363856a35e81f9b9278ebc22a8e68d2644c12
Prateek Bhatnagar <prateek89born@gmail.com>
2017-05-15 21:04:08 +0530

    full expample of webpack (#514)
    * full expample of webpack
    
    * fiing indentation
    
    * indent fix

--- packages/workbox-webpack-plugin/index.js
+++ packages/workbox-webpack-plugin/index.js
@@ -3,33 +3,41 @@ const swBuild = require('workbox-build');
 /**
  * Use the instance of this in the plugins array of the webpack config.
  * @example
- * const SwBuildWebpackPlugin = require('workbox-webpack-plugin');
+ * const WorkboxBuildWebpackPlugin = require('workbox-webpack-plugin');
  * .
  * .
- * plugins: [
- * 	new SwBuildWebpackPlugin({
- * 		rootDirectory: './build/',
- * 		swDest: './build/sw.js',
- * 		globPatterns: ['**\/*.{html,js,css}'],
- * 		globIgnores: ['admin.html'],
- * 		templatedUrls: {
- * 			'/shell': ['shell.hbs', 'main.css', 'shell.css'],
- * 		},
- * 	});
- * ]
+ * module.exports = {
+ *   entry: {
+ *     app: './step1/app.js'
+ *   },
+ *   output: {
+ *     path: __dirname + '/step1/public/js',
+ *     publicPath: '/public/js/',
+ *     filename: '[name].js',
+ *   },
+ *   plugins: [
+ *   	new WorkboxBuildWebpackPlugin({
+ *      globDirectory: './build/',
+ *      staticFileGlobs: ['**\/*.{html,js,css}'],
+ *      globIgnores: ['admin.html'],
+ *      swSrc: './src/sw.js',
+ *      swDest: './build/sw.js',
+ *   	});
+ *   ]
+ * }
  *
- * @class SwBuildWebpackPlugin
+ * @class WorkboxBuildWebpackPlugin
  */
-class SwBuildWebpackPlugin {
-	/**
-	 * Creates an instance of SwBuildWebpackPlugin.
-	 *
-	 * @param {Object} [config] all the options as passed to `swbuild`
-	 * @memberOf SwBuildWebpackPlugin
-	 */
-	constructor(config) {
-		this._config = config || {};
-	}
+class WorkboxBuildWebpackPlugin {
+  /**
+   * Creates an instance of WorkboxBuildWebpackPlugin.
+   *
+   * @param {Object} [config] all the options as passed to `swbuild`
+   * @memberOf WorkboxBuildWebpackPlugin
+   */
+  constructor(config) {
+    this._config = config || {};
+  }
 
   /**
    * @private
@@ -37,37 +45,37 @@ class SwBuildWebpackPlugin {
    * passed from Webpack to this plugin.
    * @return {Object} The configuration for a given compilation.
    */
-	getConfig(compilation) {
-		let config = this._config;
+  getConfig(compilation) {
+    let config = this._config;
 
-		// If no root directory is given, fallback to
-		// output path directory of webpack
-		if (!config.rootDirectory) {
-			config.rootDirectory = compilation.options.output.path;
-		}
+    // If no root directory is given, fallback to
+    // output path directory of webpack
+    if (!config.rootDirectory) {
+      config.rootDirectory = compilation.options.output.path;
+    }
 
-		return config;
-	}
+    return config;
+  }
 
-	/**
-	 * @param {Object} [compiler] default compiler object passed from webpack
-	 *
-	 * @memberOf SwBuildWebpackPlugin
-	 */
-	apply(compiler) {
-		compiler.plugin('after-emit', (compilation, callback) => {
-			const config = this.getConfig(compilation);
-			if (config.swSrc) {
-				swBuild.injectManifest(config)
-					.then(() => callback())
-					.catch((e) => callback(e));
-			} else {
-				swBuild.generateSW(config)
-				.then(() => callback())
-				.catch((e) => callback(e));
-			}
-		});
-	}
+  /**
+   * @param {Object} [compiler] default compiler object passed from webpack
+   *
+   * @memberOf WorkboxBuildWebpackPlugin
+   */
+  apply(compiler) {
+    compiler.plugin('after-emit', (compilation, callback) => {
+      const config = this.getConfig(compilation);
+      if (config.swSrc) {
+        swBuild.injectManifest(config)
+          .then(() => callback())
+          .catch((e) => callback(e));
+      } else {
+        swBuild.generateSW(config)
+        .then(() => callback())
+        .catch((e) => callback(e));
+      }
+    });
+  }
 }
 
-module.exports = SwBuildWebpackPlugin;
+module.exports = WorkboxBuildWebpackPlugin;
