d8d52dfd5c3bdecd1110647232b484045154c9ae
Matt Gaunt <matt@gauntface.co.uk>
2017-05-14 21:42:48 -0700

    Adding unit test for error case this PR fixes

--- packages/workbox-build/test/node/lib-write-sw.js
+++ packages/workbox-build/test/node/lib-write-sw.js
@@ -863,4 +863,65 @@ workboxSW.precache(fileManifest);
         ignoreUrlParametersMatching: [/^example/, /^other/],
       });
   });
+
+  it('should correctly handle diff sw output from globDirectory output', function() {
+      const EXPECTED_RESULT = `importScripts('workbox-sw.min.js');
+
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+const fileManifest = [
+  {
+    "url": "/",
+    "revision": "1234"
+  }
+];
+
+const workboxSW = new self.WorkboxSW();
+workboxSW.precache(fileManifest);
+`;
+    const writeSw = proxyquire('../../src/lib/write-sw', {
+      'mkdirp': {
+        sync: () => {
+          return;
+        },
+      },
+      'fs': {
+        writeFile: (filepath, stringToWrite, cb) => {
+          if (stringToWrite === EXPECTED_RESULT) {
+            cb();
+          } else {
+            stringToWrite.should.equal(EXPECTED_RESULT);
+            cb(new Error('Unexpected result from fs.'));
+          }
+        },
+      },
+    });
+
+    return writeSw(
+      'sw-path/diff-to-glob-directory/sw.js',
+      [
+        {
+          url: '/',
+          revision: '1234',
+        },
+      ],
+      'workbox-sw.min.js',
+      'glob-path/diff-to-sw-directory/'
+    );
+  });
 });
