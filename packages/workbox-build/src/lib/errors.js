99c0b3f1fc46294568efafc7d19cc0d27661b287
Gandharv Kumar Garg <gandharvkumargarg@gmail.com>
2017-05-22 18:27:43 +0530

    Update errors.js

--- packages/workbox-build/src/lib/errors.js
+++ packages/workbox-build/src/lib/errors.js
@@ -2,7 +2,7 @@ module.exports = {
   'unable-to-get-rootdir': 'Unable to get the root directory of your web app.',
   'no-extension': 'Unable to detect a usable extension for a file in your ' +
     'web app directory.',
-  'invalid-file-manifest-name': 'The File Manifest Name must have at lease ' +
+  'invalid-file-manifest-name': 'The File Manifest Name must have atleast ' +
     'one character.',
   'unable-to-get-file-manifest-name': 'Unable to get a file manifest name.',
   'invalid-sw-dest': `The 'swDest' value must be a valid path.`,
