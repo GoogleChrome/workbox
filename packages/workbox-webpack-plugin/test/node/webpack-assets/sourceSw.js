530acdf5d8ee6dc874cd3d82ab6e44c9fb6c30f4
Prateek Bhatnagar <prateek89born@gmail.com>
2017-05-25 16:00:33 +0530

    adding webpack as dev dependency

--- packages/workbox-webpack-plugin/test/node/webpack-assets/sourceSw.js
+++ packages/workbox-webpack-plugin/test/node/webpack-assets/sourceSw.js
@@ -1,5 +1,5 @@
 /* globals workboxSW */
-
+// Dummy sw for testing the generated output
 importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-messaging.js');
 workboxSW.precache([]);
--- packages/workbox-webpack-plugin/test/node/webpack-assets/src.js
+++ packages/workbox-webpack-plugin/test/node/webpack-assets/src.js
@@ -1,3 +1,5 @@
+// Dummy jS file which is to be served as an entry point for webpack
+
 (function foo() {
   return 'bar';
 })();
