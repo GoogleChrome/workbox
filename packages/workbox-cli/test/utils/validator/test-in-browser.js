a5793e1bd99a6d594dd9a54fb4a32496352b092a
Matt Gaunt <matt@gauntface.co.uk>
2017-06-07 11:02:47 -0700

    Fingers crossed this is the last issue

--- packages/workbox-cli/test/utils/validator/test-in-browser.js
+++ packages/workbox-cli/test/utils/validator/test-in-browser.js
@@ -7,13 +7,15 @@ const url = require('url');
 const getSeleniumBrowser = () => {
   if (process.platform !== 'win32') {
     console.log('Running in Chrome stable.');
-    return seleniumAssistant.getLocalBrowser('chrome', 'stable');
+    return Promise.resolve(
+      seleniumAssistant.getLocalBrowser('chrome', 'stable')
+    );
   }
 
   if (!process.env['SAUCELABS_USERNAME'] ||
     !process.env['SAUCELABS_ACCESS_KEY']) {
     console.warn('Skipping SauceLabs tests due to no credentials in environment');
-    return null;
+    return Promise.resolve(null);
   }
 
   console.log('Running in a windows Windows Environment, using SauceLabs.');
@@ -28,92 +30,94 @@ const getSeleniumBrowser = () => {
 };
 
 const testInBrowser = (baseTestUrl, fileManifestOutput, swDest, exampleProject) => {
-  const assistantBrowser = getSeleniumBrowser();
-  if (!assistantBrowser) {
-    return Promise.resolve();
-  }
-
-  let globalDriver;
-
-  const performCleanup = (err) => {
-    console.log('Performing cleanup of selenium browser...');
-    return seleniumAssistant.stopSaucelabsConnect()
-    .then(() => {
-      if (!globalDriver) {
-        return;
-      }
-
-      return seleniumAssistant.killWebDriver(globalDriver);
-    })
-    .catch(() => {})
-    .then(() => {
-      if (err) {
-        return Promise.reject(err);
-      }
-
+  return getSeleniumBrowser()
+  .then((assistantBrowser) => {
+    if (!assistantBrowser) {
       return Promise.resolve();
-    });
-  };
+    }
 
-  return assistantBrowser.getSeleniumDriver()
-  .then((browserDriver) => {
-    globalDriver = browserDriver;
-  })
-  .then(() => {
-    const urlFriendlyDest = querystring.escape(swDest);
-    console.log(`Opening URL in browser: ${baseTestUrl}/index.html?sw=${urlFriendlyDest}`);
-    return globalDriver.get(`${baseTestUrl}/index.html?sw=${urlFriendlyDest}`);
-  })
-  .then(() => {
-    console.log('Waiting for browser to have window.__testresult set....');
-    return globalDriver.wait(() => {
+    let globalDriver;
+
+    const performCleanup = (err) => {
+      console.log('Performing cleanup of selenium browser...');
+      return seleniumAssistant.stopSaucelabsConnect()
+      .then(() => {
+        if (!globalDriver) {
+          return;
+        }
+
+        return seleniumAssistant.killWebDriver(globalDriver);
+      })
+      .catch(() => {})
+      .then(() => {
+        if (err) {
+          return Promise.reject(err);
+        }
+
+        return Promise.resolve();
+      });
+    };
+
+    return assistantBrowser.getSeleniumDriver()
+    .then((browserDriver) => {
+      globalDriver = browserDriver;
+    })
+    .then(() => {
+      const urlFriendlyDest = querystring.escape(swDest);
+      console.log(`Opening URL in browser: ${baseTestUrl}/index.html?sw=${urlFriendlyDest}`);
+      return globalDriver.get(`${baseTestUrl}/index.html?sw=${urlFriendlyDest}`);
+    })
+    .then(() => {
+      console.log('Waiting for browser to have window.__testresult set....');
+      return globalDriver.wait(() => {
+        return globalDriver.executeScript(() => {
+          return typeof window.__testresult !== 'undefined';
+        });
+      });
+    })
+    .then(() => {
+      console.log('Getting window.__testresult from browser....');
       return globalDriver.executeScript(() => {
-        return typeof window.__testresult !== 'undefined';
+        return window.__testresult;
       });
-    });
-  })
-  .then(() => {
-    console.log('Getting window.__testresult from browser....');
-    return globalDriver.executeScript(() => {
-      return window.__testresult;
-    });
-  })
-  .then((testResult) => {
-    console.log('Retrieved test results from the browser...');
-    if (!testResult.entries) {
-        throw new Error(`Bad test results from mocha: '${JSON.stringify(testResult)}'`);
-      }
-
-      const entries = testResult.entries;
-      entries.length.should.equal(fileManifestOutput.length);
-
-      const pathnames = entries.map((entry) => {
-        return url.parse(entry).pathname;
-      });
-
-      fileManifestOutput.forEach((details) => {
-        try {
-          fs.statSync(path.join(exampleProject, details.url));
-        } catch (err) {
-          throw new Error(`The path '${details.url}' from the manifest doesn't seem valid.`);
+    })
+    .then((testResult) => {
+      console.log('Retrieved test results from the browser...');
+      if (!testResult.entries) {
+          throw new Error(`Bad test results from mocha: '${JSON.stringify(testResult)}'`);
         }
 
-        const expectedFileIndex = pathnames.indexOf(details.url);
-        if (expectedFileIndex === -1) {
-          console.log(entries);
-          console.log('Problem file: ', details.url);
-          throw new Error(`Unexpected file in manifest (2): '${details.url}'`);
-        }
+        const entries = testResult.entries;
+        entries.length.should.equal(fileManifestOutput.length);
 
-        pathnames.splice(expectedFileIndex, 1);
+        const pathnames = entries.map((entry) => {
+          return url.parse(entry).pathname;
+        });
 
-        (typeof details.revision).should.equal('string');
-        details.revision.length.should.be.gt(0);
-      });
+        fileManifestOutput.forEach((details) => {
+          try {
+            fs.statSync(path.join(exampleProject, details.url));
+          } catch (err) {
+            throw new Error(`The path '${details.url}' from the manifest doesn't seem valid.`);
+          }
 
-      pathnames.length.should.equal(0);
-  })
-  .then(performCleanup, performCleanup);
+          const expectedFileIndex = pathnames.indexOf(details.url);
+          if (expectedFileIndex === -1) {
+            console.log(entries);
+            console.log('Problem file: ', details.url);
+            throw new Error(`Unexpected file in manifest (2): '${details.url}'`);
+          }
+
+          pathnames.splice(expectedFileIndex, 1);
+
+          (typeof details.revision).should.equal('string');
+          details.revision.length.should.be.gt(0);
+        });
+
+        pathnames.length.should.equal(0);
+    })
+    .then(performCleanup, performCleanup);
+  });
 };
 
 module.exports = testInBrowser;
