e7b38c3fe1335d1c1294086f4b8e093b9face6d8
Jeff Posnick <jeffy@google.com>
2017-06-19 17:19:01 -0400

    Makes it easier to use custom Routes within WorkboxSW.

--- packages/workbox-routing/test/sw/router.js
+++ packages/workbox-routing/test/sw/router.js
@@ -56,4 +56,47 @@ describe('Test of the Router class', function() {
     expect(router._routes.get('GET')).to.have.members([getRoute1]);
     expect(router._routes.get('PUT')).to.have.members([putRoute1]);
   });
+
+  it(`should fail an assertion when registerRoute() is called with an invalid route`, function() {
+    const router = new workbox.routing.Router();
+    const route = {};
+    try {
+      router.registerRoute({route});
+      throw new Error();
+    } catch(error) {
+      expect(error.name).to.eql('assertion-failed',
+        `The expected assertion-failed error wasn't thrown.`);
+    }
+  });
+
+  it(`should fail an assertion when registerRoutes() is called with one invalid route`, function() {
+    const router = new workbox.routing.Router();
+    const routes = [
+      new workbox.routing.Route({match: MATCH, handler: HANDLER}),
+      {},
+    ];
+
+    try {
+      router.registerRoutes({routes});
+      throw new Error();
+    } catch(error) {
+      expect(error.name).to.eql('assertion-failed',
+        `The expected assertion-failed error wasn't thrown.`);
+    }
+  });
+
+  it(`should succeed when registerRoute() is called with something matching the Route interface`, function() {
+    const router = new workbox.routing.Router();
+    const route = {method: 'GET', match: MATCH, handler: HANDLER};
+    router.registerRoute({route});
+  });
+
+  it(`should succeed when registerRoutes() is called with multiple things matching the Route interface`, function() {
+    const router = new workbox.routing.Router();
+    const routes = [
+      new workbox.routing.Route({match: MATCH, handler: HANDLER}),
+      {method: 'GET', match: MATCH, handler: HANDLER},
+    ];
+    router.registerRoutes({routes});
+  });
 });
