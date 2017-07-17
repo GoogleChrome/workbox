952ab62d3fcdcc33f38c4bebb894ecd9255be5b4
Matt Gaunt <matt@gauntface.co.uk>
2017-07-17 13:38:10 -0700

    Copies sourcemap over with source files

--- packages/workbox-build/src/lib/utils/copy-workbox-sw.js
+++ packages/workbox-build/src/lib/utils/copy-workbox-sw.js
@@ -8,7 +8,10 @@ module.exports = (outputDirectory) => {
   const workboxSWSrcPath = require.resolve('workbox-sw');
   const workboxSWDestPath = path.join(outputDirectory,
     path.basename(workboxSWSrcPath));
-  return fse.copy(workboxSWSrcPath, workboxSWDestPath)
+  return Promise.all([
+      fse.copy(workboxSWSrcPath, workboxSWDestPath),
+      fse.copy(`${workboxSWSrcPath}.map`, `${workboxSWDestPath}.map`),
+    ])
     .then(() => workboxSWDestPath)
     .catch((error) => {
       throw Error(`${errors['unable-to-copy-workbox-sw']} ${error}`);
