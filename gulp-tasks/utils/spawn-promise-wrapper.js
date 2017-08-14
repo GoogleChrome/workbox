0fde1f9c774bf915e356340e1296d8b0b6b16a06
Matt Gaunt <matt@gauntface.co.uk>
2017-08-14 09:56:22 -0700

    Removing unnessecary change in spawn wrapper

--- gulp-tasks/utils/spawn-promise-wrapper.js
+++ gulp-tasks/utils/spawn-promise-wrapper.js
@@ -2,9 +2,7 @@ const spawn = require('child_process').spawn;
 
 module.exports = (command, args) => {
   return new Promise((resolve, reject) => {
-    const process = spawn(command, args, {
-      stdio: ['ignore', 'inherit', 'inherit'],
-    });
+    const process = spawn(command, args, {stdio: 'inherit'});
     process.on('error', reject);
     process.on('close', (code) => {
       if (code === 0) {
