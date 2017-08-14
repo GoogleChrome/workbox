82a92ed2d432b3609d01a2ae913af910133173a1
Matt Gaunt <matt@gauntface.co.uk>
2017-08-14 12:30:39 -0700

    Upload to CDN

--- gulp-tasks/publish.js
+++ gulp-tasks/publish.js
@@ -1,29 +1,58 @@
 const gulp = require('gulp');
+const semver = require('semver');
+const remoteGitTags = require('remote-git-tags');
 
 const spawnPromiseWrapper = require('./utils/spawn-promise-wrapper');
-const remoteGitTags = require('remote-git-tags');
+
+const PROJECT_ID = 'workbox-bab1f';
+const BUCKET_NAME = 'workbox-cdn';
 
 gulp.task('publish:lerna', () => {});
 
-gulp.task('publish:cdn', () => {
-  const PROJECT_ID = 'workbox-bab1f';
-  const BUCKET_NAME = 'workbox-cdn';
+const uploadToCDN = (directoryToUpload, uploadDirectory) => {
+  return spawnPromiseWrapper('gcloud', [
+    'config', 'set', 'project', PROJECT_ID,
+  ])
+  .then(() => {
+    return spawnPromiseWrapper('gsutil', [
+      'cp', ``, `gs://${BUCKET_NAME}/${tag}/`,
+    ]);
+  });
+};
 
+const publishTagToCDN = (tag) => {
+  // TODO: Download Tag
+
+  // TODO: Upload tagged files to CDN
+
+  return Promise.resolve();
+};
+
+gulp.task('publish:cdn', () => {
   const GIT_REPO = 'github.com/GoogleChrome/workbox';
 
   return remoteGitTags(GIT_REPO)
   .then((tags) => {
-    console.log('Tags: ', tags);
+    const tagObject = {};
+    tags.forEach((value, key) => {
+      tagObject[key] = value;
+    });
+    return Object.keys(tagObject);
+  })
+  .then((tags) => {
+    return tags.filter((tag) => semver.gt(tag, '3.0.0'));
+  })
+  .then((tagsToPublish) => {
+    return tagsToPublish.reduce((promiseChain, tag) => {
+      return promiseChain.then(() => {
+        return publishTagToCDN(tag);
+      });
+    }, Promise.resolve());
   })
   .then(() => {
-    /** return spawnPromiseWrapper('gcloud', [
-      'config', 'set', 'project', PROJECT_ID,
-    ])**/
-  })
-  .then(() => {
-    /** spawnPromiseWrapper('gsutil', [
-      'cp', ``, `gs://${BUCKET_NAME}/${GIT_TAG}/`
-    ]);**/
+    // TODO: Remove this once v3 is launched.
+    // This publishes 'v3.0.0-alpha' as a release
+    uploadToCDN('', 'v3.0.0-alpha.0');
   });
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ const gulpTaskFiles = [
   'lint',
   'test',
   'analyze-properties',
+  'publish',
 ];
 
 gulpTaskFiles.forEach((gulpTaskFile) => {
