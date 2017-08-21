1b9d4f69fe206af93836ed15024076eeda9b95de
Matt Gaunt <matt@gauntface.co.uk>
2017-08-21 16:40:30 -0700

    Removing commented out code

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -2,30 +2,9 @@ const gulp = require('gulp');
 const oneLine = require('common-tags').oneLine;
 const spawn = require('./utils/spawn-promise-wrapper');
 
-// const constants = require('./utils/constants');
 const packageRunnner = require('./utils/package-runner');
 const logHelper = require('./utils/log-helper');
 const pkgPathToName = require('./utils/pkg-path-to-name');
-
-/** const runBundledTests = (packagePath, env) => {
-  logHelper.log(oneLine`
-    Running Test Bundles for
-    ${logHelper.highlight(pkgPathToName(packagePath))}.
-  `);
-
-  const mochaOptions = {
-    reporter: path.join(__dirname, './utils/mocha-coverage-reporter'),
-  };
-  if (global.cliOptions.grep) {
-    mochaOptions.grep = global.cliOptions.grep;
-  }
-
-  const bundleTestDirectory = path.posix.join(
-    'test', pkgPathToName(packagePath), constants.TEST_BUNDLES_BUILD_DIRNAME);
-  return gulp.src(
-    path.posix.join(bundleTestDirectory, env, '*.js'), {read: false})
-  .pipe(mocha(mochaOptions));
-};**/
 
 const runNodeTests = (packagePath, buildType) => {
   logHelper.log(oneLine`
@@ -44,7 +23,6 @@ gulp.task('test:node', gulp.series(
 ));
 
 gulp.task('test', gulp.series(
-  // 'build-test-bundles',
   'test:node',
   'lint'
 ));
