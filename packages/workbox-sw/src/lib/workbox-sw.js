30f09c640548a53f9fc0bc95d5c11b22ff04da66
Jeffrey Posnick <jeffy@google.com>
2017-09-07 03:09:04 +0900

    Ensure that ignoreUrlParametersMatching works with directoryIndex (#762)
    * Ensure that ignoreUrlParametersMatching works with directoryIndex
    
    * Changed the stubbing a bit to make Firefox happy.
    
    * Cleaned up the test a bit.
    
    * Add in checks for the cache.match() arguments.

--- packages/workbox-sw/src/lib/workbox-sw.js
+++ packages/workbox-sw/src/lib/workbox-sw.js
@@ -332,8 +332,8 @@ class WorkboxSW {
       }
 
       if (directoryIndex && strippedUrl.pathname.endsWith('/')) {
-        url.pathname += directoryIndex;
-        return cachedUrls.indexOf(url.href) !== -1;
+        strippedUrl.pathname += directoryIndex;
+        return cachedUrls.indexOf(strippedUrl.href) !== -1;
       }
 
       return false;
