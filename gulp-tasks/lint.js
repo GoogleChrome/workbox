1b83189484034850b17cdaebfa5d5c5c7dceea33
Matt Gaunt <matt@gauntface.co.uk>
2017-09-14 15:43:42 -0700

    Fixing windows builds hopefully

--- gulp-tasks/lint.js
+++ gulp-tasks/lint.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp');
 const spawn = require('./utils/spawn-promise-wrapper');
+const getNpmCmd = require('./utils/get-npm-cmd');
 const logHelper = require('../infra/utils/log-helper');
 
 // Use npm run lint to ensure we are using local eslint
 gulp.task('lint', () => {
-  return spawn('npm', ['run', 'lint'])
+  return spawn(getNpmCmd(), ['run', 'lint'])
   .catch((err) => {
     logHelper.error(err);
     throw new Error(`[Workbox Error Msg] 'gulp lint' discovered errors.`);
