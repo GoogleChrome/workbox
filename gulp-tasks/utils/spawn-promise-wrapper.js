e9aebb349d4178c7cc107f47a62e94d23f13addf
Matt Gaunt <matt@gauntface.co.uk>
2017-09-14 13:42:30 -0700

    Adding log to spawn command on windows

--- gulp-tasks/utils/spawn-promise-wrapper.js
+++ gulp-tasks/utils/spawn-promise-wrapper.js
@@ -1,9 +1,10 @@
 const spawn = require('child_process').spawn;
+const logHelper = require('../../infra/utils/log-helper');
 
 module.exports = (command, args, options = {}) => {
   options.stdio = options.stdio || 'inherit';
   return new Promise((resolve, reject) => {
-    console.log(command);
+    logHelper.log(`Spawning command: `, command);
     const process = spawn(command, args, options);
     process.on('error', reject);
     process.on('close', (code) => {
