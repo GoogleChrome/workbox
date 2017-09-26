1e355ea5042b3fd8ccf1b96912f29c9611966578
Matt Gaunt <matt@gauntface.co.uk>
2017-09-26 16:59:46 -0700

    Tweaking tests to not use the iife (#820)

--- test/workbox-routing/node/normalizeHandler.mjs
+++ test/workbox-routing/node/normalizeHandler.mjs
@@ -1,83 +1,99 @@
+import clearRequire from 'clear-require';
 import makeServiceWorkerEnv from 'service-worker-mock';
 import {expect} from 'chai';
 
 import expectError from '../../../infra/utils/expectError.js';
 
-(async () => {
-  Object.assign(global, makeServiceWorkerEnv());
+Object.assign(global, makeServiceWorkerEnv());
 
-  const normalizeHandler = (await import('../../../packages/workbox-routing/lib/normalizeHandler.mjs')).default;
+const handler = {
+  handle: () => {},
+};
+const functionHandler = () => {};
 
-  const handler = {
-    handle: () => {},
-  };
-  const functionHandler = () => {};
+const invalidHandlerObject = {};
+const invalidHandlerString = 'INVALID';
 
-  const invalidHandlerObject = {};
-  const invalidHandlerString = 'INVALID';
+describe(`workbox-routing: _normalizeHandler`, function() {
+  const initialNodeEnv = process.env.NODE_ENV;
 
-  describe(`workbox-routing: _normalizeHandler`, function() {
-    const initialNodeEnv = process.env.NODE_ENV;
-    after(function() {
-      process.env.NODE_ENV = initialNodeEnv;
+  after(function() {
+    process.env.NODE_ENV = initialNodeEnv;
+  });
+
+  describe(`(NODE_ENV = development)`, function() {
+    let normalizeHandler;
+
+    before(async function() {
+      process.env.NODE_ENV = 'development';
+
+      clearRequire.all();
+
+      // We need to import after setting the NODE_ENV so workbox-core also
+      // gets the correct NODE_ENV
+      const normalizeModule = await import('../../../packages/workbox-routing/lib/normalizeHandler.mjs');
+      normalizeHandler = normalizeModule.default;
     });
 
-    describe(`(NODE_ENV = development)`, function() {
-      before(function() {
-        process.env.NODE_ENV = 'development';
-      });
-
-      it(`should properly normalize an object that exposes a handle method`, async function() {
-        const normalizedHandler = normalizeHandler(handler);
-        expect(normalizedHandler).to.have.property('handle');
-      });
-
-      it(`should properly normalize a function`, async function() {
-        const normalizedHandler = normalizeHandler(functionHandler);
-        expect(normalizedHandler).to.have.property('handle');
-      });
-
-      it(`should throw when called with an object that doesn't expose a handle method`, async function() {
-        await expectError(
-          () => normalizeHandler(invalidHandlerObject),
-          'missing-a-method',
-          (error) => {
-            expect(error.details).to.have.property('moduleName').that.equals('workbox-routing');
-            expect(error.details).to.have.property('className').that.equals('Route');
-            expect(error.details).to.have.property('funcName').that.equals('constructor');
-            expect(error.details).to.have.property('paramName').that.equals('handler');
-          }
-        );
-      });
-
-      it(`should throw when called with something other than a function or an object`, async function() {
-        await expectError(
-          () => normalizeHandler(invalidHandlerString),
-          'incorrect-type',
-          (error) => {
-            expect(error.details).to.have.property('moduleName').that.equals('workbox-routing');
-            expect(error.details).to.have.property('className').that.equals('Route');
-            expect(error.details).to.have.property('funcName').that.equals('constructor');
-            expect(error.details).to.have.property('paramName').that.equals('handler');
-          }
-        );
-      });
+    it(`should properly normalize an object that exposes a handle method`, function() {
+      const normalizedHandler = normalizeHandler(handler);
+      expect(normalizedHandler).to.have.property('handle');
     });
 
-    describe(`(NODE_ENV = production)`, function() {
-      before(function() {
-        process.env.NODE_ENV = 'production';
-      });
+    it(`should properly normalize a function`, async function() {
+      const normalizedHandler = normalizeHandler(functionHandler);
+      expect(normalizedHandler).to.have.property('handle');
+    });
 
-      it(`should properly normalize an object that exposes a handle method`, async function() {
-        const normalizedHandler = normalizeHandler(handler);
-        expect(normalizedHandler).to.have.property('handle');
-      });
+    it(`should throw when called with an object that doesn't expose a handle method`, function() {
+      return expectError(
+        () => normalizeHandler(invalidHandlerObject),
+        'missing-a-method',
+        (error) => {
+          expect(error.details).to.have.property('moduleName').that.equals('workbox-routing');
+          expect(error.details).to.have.property('className').that.equals('Route');
+          expect(error.details).to.have.property('funcName').that.equals('constructor');
+          expect(error.details).to.have.property('paramName').that.equals('handler');
+        }
+      );
+    });
 
-      it(`should properly normalize a function`, async function() {
-        const normalizedHandler = normalizeHandler(functionHandler);
-        expect(normalizedHandler).to.have.property('handle');
-      });
+    it(`should throw when called with something other than a function or an object`, function() {
+      return expectError(
+        () => normalizeHandler(invalidHandlerString),
+        'incorrect-type',
+        (error) => {
+          expect(error.details).to.have.property('moduleName').that.equals('workbox-routing');
+          expect(error.details).to.have.property('className').that.equals('Route');
+          expect(error.details).to.have.property('funcName').that.equals('constructor');
+          expect(error.details).to.have.property('paramName').that.equals('handler');
+        }
+      );
     });
   });
-})();
+
+  describe(`(NODE_ENV = production)`, function() {
+    let normalizeHandler;
+
+    before(async function() {
+      process.env.NODE_ENV = 'production';
+
+      clearRequire.all();
+
+      // We need to import after setting the NODE_ENV so workbox-core also
+      // gets the correct NODE_ENV
+      const normalizeModule = await import('../../../packages/workbox-routing/lib/normalizeHandler.mjs');
+      normalizeHandler = normalizeModule.default;
+    });
+
+    it(`should properly normalize an object that exposes a handle method`, function() {
+      const normalizedHandler = normalizeHandler(handler);
+      expect(normalizedHandler).to.have.property('handle');
+    });
+
+    it(`should properly normalize a function`, function() {
+      const normalizedHandler = normalizeHandler(functionHandler);
+      expect(normalizedHandler).to.have.property('handle');
+    });
+  });
+});
