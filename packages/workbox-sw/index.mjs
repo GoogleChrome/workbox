ac18849996f8c3f107035458750874fd3763377f
Matt Gaunt <matt@gauntface.co.uk>
2017-10-03 13:44:26 -0700

    Adding start of workbox-sw

---
+++ packages/workbox-sw/index.mjs
@@ -0,0 +1,42 @@
+/**
+ * This module is a single import that can be used to dynamically import
+ * additional Workbox modules with no effort.
+ *
+ * @module workbox-sw
+ */
+
+/**
+ * This class can be used to make it easy to use the various parts of
+ * Workbox.
+ */
+class WorkboxSW {
+  /**
+   * You can specify whether to treat as a debug
+   * build and whether to use a CDN or a specific path when importing other
+   * workbox-modules
+   *
+   * @param {Object} options
+   */
+  constructor(options) {
+    this._options = Object.assign({
+      debug: location.hostname === 'localhost',
+      pathPrefix: null,
+    }, options);
+  }
+
+  /**
+   * Force a service worker to become active, instead of waiting, and
+   * to claim any available clients.
+   */
+  skipAndClaim() {
+    self.addEventListener('install', (event) => {
+      event.waitUntil(self.skipWaiting());
+    });
+
+    self.addEventListener('activate', (event) => {
+      event.waitUntil(self.clientsClaim());
+    });
+  }
+}
+
+export default WorkboxSW;
