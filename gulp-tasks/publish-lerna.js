613cecedc9d667d66d0ad541594d1b023080fe3e
Matt Gaunt <matt@gauntface.co.uk>
2017-10-10 17:49:39 -0400

    Bumping lerna and switching to version

--- gulp-tasks/publish-lerna.js
+++ gulp-tasks/publish-lerna.js
@@ -3,39 +3,18 @@ const gulp = require('gulp');
 const getNpmCmd = require('./utils/get-npm-cmd');
 const spawn = require('./utils/spawn-promise-wrapper');
 
-gulp.task('publish-lerna:no-push', () => {
+gulp.task('publish-lerna', () => {
   return spawn(getNpmCmd(), [
     'run', 'local-lerna',
     '--',
     'publish',
 
-    // First run lerna and get it to update the relvant packages.
-    '--skip-git',
-    '--skip-npm',
-
     // TODO: The following flags are for testing deploy process and can
     // be removed
 
+    '--skip-git',
+    '--skip-npm',
     '--cd-version=prerelease', '--preid=alpha',
-    '--npm-tag', 'alpha',
-  ]);
-});
-
-gulp.task('publish-lerna:push', () => {
-  const lernaPkg = require('../lerna.json');
-  return spawn(getNpmCmd(), [
-    'run', 'local-lerna',
-    '--',
-    'publish',
-    // Then skip all questions
-    '--yes',
-    // force use of the versino in lernaPkg
-    '--repo-version', lernaPkg.version,
-
-    // TODO: The following flags are for testing deploy process and can
-    // be removed
-    '--skip-git',
-    '--skip-npm',
     '--npm-tag', 'alpha',
   ]);
 });
--- gulp-tasks/publish.js
+++ gulp-tasks/publish.js
@@ -4,26 +4,6 @@ const fs = require('fs-extra');
 
 const constants = require('./utils/constants');
 
-const lernaPath = path.join(__dirname, '../lerna.json');
-let lernaVersion;
-
-// The get-current-version and check-version tasks exist incase the user
-// skips / ctrl + c's the first lerna publish command
-gulp.task('publish:get-current-version', () => {
-  const lernaPkg = JSON.parse(fs.readFileSync(lernaPath).toString());
-  lernaVersion = lernaPkg.version;
-  return Promise.resolve();
-});
-
-gulp.task('publish:check-version', () => {
-  // Read file from filesystem to avoid require caching
-  const lernaPkg = JSON.parse(fs.readFileSync(lernaPath).toString());
-  if (lernaPkg.version === lernaVersion) {
-    return Promise.reject(`Lerna version wasn't updated.`);
-  }
-  return Promise.resolve();
-});
-
 gulp.task('publish:clean', () => {
   return fs.remove(path.join(__dirname, '..',
     constants.GENERATED_RELEASE_FILES_DIRNAME));
@@ -36,10 +16,7 @@ gulp.task('publish:cdn+git', gulp.series([
 ]));
 
 gulp.task('publish', gulp.series([
-  'test',
-  'publish:get-current-version',
-  'publish-lerna:no-push',
-  'publish:check-version',
-  'publish-lerna:push',
+  // 'test',
+  'publish-lerna',
   'publish:cdn+git',
 ]));
