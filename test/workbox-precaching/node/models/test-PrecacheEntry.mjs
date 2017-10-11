4153f4dca4b0dc8a2dd1e44d6efbc959c77a9cd5
Matt Gaunt <matt@gauntface.co.uk>
2017-10-11 10:13:28 -0400

    Fixing up tests

--- test/workbox-precaching/node/models/test-PrecacheEntry.mjs
+++ test/workbox-precaching/node/models/test-PrecacheEntry.mjs
@@ -13,12 +13,12 @@ describe('[workbox-precaching] PrecacheEntry', function() {
   describe('constructor', function() {
     it(`should use search param if 'cache' option is not supported`, async function() {
       const entry = new PrecacheEntry('example', '/url', '1234', true);
-      expect(entry._networkRequest.url).to.equal(`${self.location}/url?_workbox-precaching=1234`);
+      expect(entry._networkRequest.url).to.equal(`${self.location}/url?_workbox-cache-bust=1234`);
     });
 
     it(`should use search param if 'cache' option is not supported and keep previous search params`, async function() {
       const entry = new PrecacheEntry('example', '/url?foo=bar', '1234', true);
-      expect(entry._networkRequest.url).to.equal(`${self.location}/url?foo=bar&_workbox-precaching=1234`);
+      expect(entry._networkRequest.url).to.equal(`${self.location}/url?foo=bar&_workbox-cache-bust=1234`);
     });
 
     it(`should use 'cache' option if supported`, async function() {
