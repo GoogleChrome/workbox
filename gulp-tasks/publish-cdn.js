a8ba06053da833ac3f54613750ccafa082b3e36d
Matt Gaunt <matt@gauntface.co.uk>
2017-10-13 12:41:35 -0400

    For the temporary github and CDN stuff, remove the generated files

--- gulp-tasks/publish-cdn.js
+++ gulp-tasks/publish-cdn.js
@@ -6,11 +6,16 @@ const cdnUploadHelper = require('./utils/cdn-helper');
 const publishHelpers = require('./utils/publish-helpers');
 const githubHelper = require('./utils/github-helper');
 const logHelper = require('../infra/utils/log-helper');
+const constants = require('./utils/constants');
 
 const findMissingCDNTags = async (tagsData) => {
   const missingTags = [];
   for (let tagData of tagsData) {
     let exists = await cdnUploadHelper.tagExists(tagData.name);
+
+    if (tagData.name.indexOf('3.0.0-alpha') !== -1) {
+      exists = false;
+    }
 
     if (!exists) {
       missingTags.push(tagData);
@@ -54,6 +59,11 @@ gulp.task('publish-cdn:generate-from-tags', async () => {
 });
 
 gulp.task('publish-cdn:temp-v3', async () => {
+  // Let's force this to always be fresh - in case we run it outside of
+  // gulp publish
+  fs.removeSync(
+    path.join(__dirname, '..', constants.GENERATED_RELEASE_FILES_DIRNAME));
+
   const lernaPkg = fs.readJSONSync(
     path.join(__dirname, '..', 'lerna.json'),
   );
