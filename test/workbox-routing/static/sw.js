952ba6479fedf8f7367f6b525f96edf34418ea7f
Matt Gaunt <matt@gauntface.co.uk>
2017-10-14 11:28:37 -0400

    Making lint happy

--- test/workbox-routing/static/sw.js
+++ test/workbox-routing/static/sw.js
@@ -1,14 +1,15 @@
 importScripts('../../../../packages/workbox-core/build/browser/workbox-core.dev.js');
 importScripts('../../../../packages/workbox-routing/build/browser/workbox-routing.dev.js');
 
-workbox.core.default.setLogLevel(
-  workbox.core.LOG_LEVELS.debug
+global.workbox.core.default.setLogLevel(
+  global.workbox.core.LOG_LEVELS.debug
 );
 
-const routing = workbox.routing.default;
+const routing = self.workbox.core.routing.default;
+const Route = self.workbox.core.routing.Route;
 
 const specialImgUrl = new URL('/static/demo/workbox-routing/demo-img.png', location).toString();
-const specialImgRoute = new workbox.routing.Route(({url}) => {
+const specialImgRoute = new Route(({url}) => {
   return (url === specialImgUrl);
 }, ()=> {
   return fetch('http://via.placeholder.com/300x300/ffffff/F57C00?text=Hello+from+Workbox');
