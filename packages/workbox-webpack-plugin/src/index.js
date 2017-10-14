9f11fac413900659c200893849d150656d1094bb
Will Farley <wfarley@amazon.com>
2017-10-14 16:00:27 -0700

    Add better documentation to webpack lifecycle methods

--- packages/workbox-webpack-plugin/src/index.js
+++ packages/workbox-webpack-plugin/src/index.js
@@ -30,8 +30,16 @@ const {setReadFile} = require('./lib/utils/read-file');
 class WorkboxWebpackPlugin {
   /**
    * Creates an instance of WorkboxBuildWebpackPlugin.
-   *
-   * @param {Object} [config] All the options as passed to `workbox-build`.
+   * @param {module:workbox-build.Configuration} [config] All the options as
+   *        passed to `workbox-build`.
+   * @param {Array<String>} [config.chunks] Array of chunk names to use for
+   *        generating the asset manifest. All assets belonging to the provided
+   *        chunk names will be included in the asset manifest. Any chunks that
+   *        are not listed or do not have a name will be removed.
+   * @param {Array<String>} [config.excludeChunks] Array of chunk names to
+   *        exclude from the asset manifest. Any asset beloning to the provided
+   *        chunk names will not be included in the asset manifest. This does
+   *        not affect chunks with no chunk name.
    */
   constructor(config) {
     this._config = config || {};
@@ -60,16 +68,37 @@ class WorkboxWebpackPlugin {
       setReadFile(readFile);
       next();
     });
+    /**
+     * During the make phase of the webpack compilation, we use
+     * workbox-webpack-plugin/utils/copy-workbox-sw to add a built version of
+     * workbox-sw to the webpack compilation assets array.
+     */
     compiler.plugin('make', (compilation, next) => {
       copyWorkboxSW(compilation.options.output.path)
       .then(({workboxSW, workboxSWMap, workboxSWName}) => {
         // Add the workbox-sw file to compilation assets
         compilation.assets[workboxSWName] = webpackAsset(workboxSW);
         compilation.assets[`${workboxSWName}.map`] = webpackAsset(workboxSWMap);
+        // The version of workbox-sw is included in it's filename so we need
+        // that information to import it in the generated service worker.
+        // workbox-webpack-plugin/utils/copy-workbox-sw also returns the
+        // workbox-sw filename so we can pass it to
+        // workbox-webpack-plugin/generate-webpack-sw.
         this.config.set('workboxSWFilename', workboxSWName);
         next();
       });
     });
+    /**
+     * During the emit phase of the webpack lifecycle, we:
+     *  1. Get the manifest entries
+     *  2. Use the entries to generate a file-manifest
+     *  3. Generate a service worker with the file-manifest name and workbox-sw
+     *     filename or copy a service worker from the config.swSrc then prepend
+     *     it with the required importScripts(workbox-sw.js, file-manifest.js).
+     *  4. Add both the file-manifest and the service-worker to the webpack
+     *     assets.
+     * @type {[type]}
+     */
     compiler.plugin('emit', (compilation, next) => {
       const config = this.config.get();
       const {
