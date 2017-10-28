0f18b2d87214cef2862152ec22471996b8f15e26
Matt Gaunt <matt@gauntface.co.uk>
2017-10-28 12:45:42 -0700

    Adding an expect

--- test/workbox-core/node/_private/indexedDBHelper.mjs
+++ test/workbox-core/node/_private/indexedDBHelper.mjs
@@ -1,9 +1,11 @@
+import {expect} from 'chai';
 import indexedDBHelper from '../../../../packages/workbox-core/_private/indexedDBHelper.mjs';
 
 describe(`[workbox-core] indexedDBHelper`, function() {
   describe('getDB()', function() {
     it(`should getDB()`, async function() {
-      await indexedDBHelper.getDB(`db-name`, 1, () => {});
+      const dbWrapper = await indexedDBHelper.getDB(`db-name`, 1, () => {});
+      expect(dbWrapper).to.exist;
     });
   });
 });
