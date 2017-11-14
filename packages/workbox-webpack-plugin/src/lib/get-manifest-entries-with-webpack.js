1d6727014dc4259128b8c22d84ec9d05396bc7b6
Jeffrey Posnick <jeffy@google.com>
2017-11-14 15:35:57 -0500

    Exclude revision info when the URL includes a known hash. (#1042)
    * Exclude revision info when the URL includes a known hash.
    
    * More tests.
    
    * Linting.

--- packages/workbox-webpack-plugin/src/lib/get-manifest-entries-with-webpack.js
+++ packages/workbox-webpack-plugin/src/lib/get-manifest-entries-with-webpack.js
@@ -17,16 +17,26 @@
 const resolveWebpackUrl = require('./utils/resolve-webpack-url');
 
 /**
- * A single manifest entry that workbox-sw can precache.
+ * A single manifest entry that Workbox can precache.
+ * When possible, we leave out the revision information, which tells Workbox
+ * that the URL contains enough info to uniquely version the asset.
  *
+ * @param {Array<string>} knownHashes All of the hashes that are associated
+ * with this webpack build.
  * @param {string} url webpack asset url path
  * @param {string} [revision] A revision hash for the entry
  * @return {module:workbox-build.ManifestEntry} A single manifest entry
  *
  * @private
  */
-function getEntry(url, revision) {
-  return revision ? {url, revision} : {url};
+function getEntry(knownHashes, url, revision) {
+  // We're assuming that if the URL contains any of the known hashes
+  // (either the short or full chunk hash or compilation hash) then it's
+  // already revisioned, and we don't need additional out-of-band revisioning.
+  if (!revision || knownHashes.some((hash) => url.includes(hash))) {
+    return {url};
+  }
+  return {revision, url};
 }
 
 /**
@@ -85,9 +95,6 @@ function mapAssetsToChunkHash(chunks) {
  *   Rename variables so they are easier to understand:
  *      https://github.com/GoogleChrome/workbox/pull/808#discussion_r139605624
  *      https://github.com/GoogleChrome/workbox/pull/808#discussion_r139605973
- *   If an asset is already using some kind of build hash, do not use the
- *   revision parameter in `entry()`:
- *      https://github.com/GoogleChrome/workbox/pull/808#discussion_r140584483
  *
  * @function getManifestEntriesWithWebpack
  * @param {Object} compiler webpack compiler
@@ -130,10 +137,20 @@ module.exports = (compiler, compilation, config) => {
     ? Object.keys(assetsToChunkHash)
     : Object.keys(compilation.assets);
 
+  const knownHashes = [compilation.hash, compilation.fullHash];
+  for (const chunk of compilation.chunks) {
+    knownHashes.push(chunk.hash, chunk.renderedHash);
+  }
+
   // Create and return the manifest entries.
   return manifestFiles.map((filePath) => {
-    return getEntry(resolveWebpackUrl(publicPath, filePath),
-      // Use chunkhash if available, otherwise use compilation hash.
-      assetsToChunkHash[filePath] || compilation.hash);
+    return getEntry(
+      // Make sure we don't have any empty/undefined hashes.
+      knownHashes.filter((hash) => !!hash),
+      resolveWebpackUrl(publicPath, filePath),
+      // Use chunkhash if available, otherwise use the hash of the file's
+      // contents, and failing that, the compilation's hash.
+      assetsToChunkHash[filePath] || compilation.hash
+    );
   });
 };
