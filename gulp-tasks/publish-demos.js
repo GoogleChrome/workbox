9795f99ab37f94661d59700a75e7f203cf0b54f9
Matt Gaunt <matt@gauntface.co.uk>
2017-11-27 10:10:38 -0800

    Removing dead code and adding TODO to change demo publish

--- gulp-tasks/publish-demos.js
+++ gulp-tasks/publish-demos.js
@@ -7,6 +7,9 @@ const getNpmCmd = require('./utils/get-npm-cmd');
 const spawn = require('./utils/spawn-promise-wrapper');
 const constants = require('./utils/constants');
 
+// TODO: This should publish based on git tags, similar to Github and CDN
+// releases
+
 gulp.task('publish-demos:updateCDNDetails', () => {
   const details = {
     latestUrl: getVersionsCDNUrl(),
