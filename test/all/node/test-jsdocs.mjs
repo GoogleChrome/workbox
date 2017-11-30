d55f188626bdca8aa74461e9fc48d298c1a28720
Matt Gaunt <matt@gauntface.co.uk>
2017-11-30 09:15:26 -0800

    Back to simpler tests

--- test/all/node/test-jsdocs.mjs
+++ test/all/node/test-jsdocs.mjs
@@ -3,7 +3,6 @@ import glob from 'glob';
 import {expect} from 'chai';
 import fs from 'fs-extra';
 import spawn from '../../../gulp-tasks/utils/spawn-promise-wrapper';
-import logHelper from '../../../infra/utils/log-helper';
 
 describe('[all] JSDocs', function() {
   it('should run JSDocs and have no unexpected results', async function() {
@@ -11,26 +10,24 @@ describe('[all] JSDocs', function() {
     this.timeout(60 * 1000);
 
     const projectRoot = path.join(__dirname, '..', '..', '..');
+    const docsPath = path.join(projectRoot, 'docs');
     await spawn('gulp', ['docs:build'], {
       cwd: projectRoot,
     });
 
-    const docs = glob.sync('docs/**/*.html', {
-      cwd: projectRoot,
+    const docs = glob.sync('*.html', {
+      cwd: docsPath,
     });
 
-    logHelper.log('Project Root path: ', projectRoot);
-    logHelper.log('Globbed Docs: ', docs);
-
     // global.html is only added when the docs have stray global values.
-    if (docs.indexOf('docs/global.html') !== -1) {
+    if (docs.indexOf('global.html') !== -1) {
       throw new Error('There should be **no** globals in the JSDocs.');
     }
 
     // On some occassions module.exports can leak into JSDocs and breaks
     // in the final template.
-    expect(docs.indexOf('docs/index-all.html')).to.not.equal(-1);
-    const indexAllContents = fs.readFileSync(path.join(projectRoot, 'docs', 'index-all.html'))
+    expect(docs.indexOf('index-all.html')).to.not.equal(-1);
+    const indexAllContents = fs.readFileSync(path.join(docsPath, 'index-all.html'))
       .toString();
     if (indexAllContents.indexOf('<a href="module.html#.exports">module.exports</a>') !== -1) {
       throw new Error('There is a stray `module.exports` in the docs. ' +
