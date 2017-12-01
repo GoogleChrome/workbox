29f3cdc73b66d1f47e38ddd79df759104bc89bfe
Matt Gaunt <matt@gauntface.co.uk>
2017-12-01 15:07:26 -0800

    Github publish flow fixes. (#1099)
    * Should be good to go
    
    * Mark as a draft
    
    * prelease can be smarted ;)

--- gulp-tasks/publish-github.js
+++ gulp-tasks/publish-github.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const path = require('path');
+const semver = require('semver');
 
 const publishHelpers = require('./utils/publish-helpers');
 const githubHelper = require('./utils/github-helper');
@@ -12,6 +13,7 @@ const publishReleaseOnGithub =
       tag_name: tagName,
       draft: true,
       name: `Workbox ${tagName}`,
+      prerelease: semver.prerelease(tagName) !== null,
     });
     releaseInfo = releaseData.data;
   }
@@ -40,9 +42,9 @@ const handleGithubRelease = async (tagName, gitBranch, releaseInfo) => {
   return publishReleaseOnGithub(tagName, releaseInfo, tarPath, zipPath);
 };
 
-const filterTagsWithReleaseBundles = (taggedReleases) => {
-  return Object.keys(taggedReleases).filter((tagName) => {
-    const release = taggedReleases[tagName];
+const filterTagsWithReleaseBundles = (allTags, taggedReleases) => {
+  return allTags.filter((tagInfo) => {
+    const release = taggedReleases[tagInfo.name];
     if (release && release.assets.length > 0) {
       // If a tag has a release and there is an asset let's assume the
       // the release is fine. Note: Github's source doesn't count as an
@@ -56,15 +58,19 @@ const filterTagsWithReleaseBundles = (taggedReleases) => {
 
 gulp.task('publish-github:generate-from-tags', async () => {
   // Get all of the tags in the repo.
+  const allTags = await githubHelper.getTags();
   const taggedReleases = await githubHelper.getTaggedReleases();
-  const tagsToBuild = await filterTagsWithReleaseBundles(taggedReleases);
+  const tagsToBuild = await filterTagsWithReleaseBundles(
+    allTags, taggedReleases);
 
   if (tagsToBuild.length === 0) {
     logHelper.log(`No tags missing from Github.`);
+    return;
   }
 
-  for (let tagName of tagsToBuild) {
-    await handleGithubRelease(tagName, tagName, taggedReleases[tagName]);
+  for (let tagInfo of tagsToBuild) {
+    await handleGithubRelease(
+      tagInfo.name, tagInfo.name, taggedReleases[tagInfo.name]);
   }
 });
 
