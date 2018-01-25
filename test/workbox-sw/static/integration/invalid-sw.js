1b1f9b8f9fe37ec1ae905bad0967adfbccfe30e6
Jeff Posnick <jeffy@google.com>
2018-01-25 11:52:50 -0500

    Linting.

--- test/workbox-sw/static/integration/invalid-sw.js
+++ test/workbox-sw/static/integration/invalid-sw.js
@@ -1,7 +1,8 @@
 importScripts('/__WORKBOX/buildFile/workbox-sw');
 
 // This is expected to lead to an error.
-const module = self.workbox['does-not-exist'];
+const namespace = 'doesnotexist';
+const module = self.workbox[namespace];
 if (!module) {
-  throw new Error(`self.workbox.${namespace} did not load the expected interface.`);
+  throw new Error(`self.workbox.${namespace} did not load anything.`);
 }
