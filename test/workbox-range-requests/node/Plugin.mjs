b23d35b520829b235c6a0aeae92f9c7849a7fcb7
Matt Gaunt <matt@gauntface.co.uk>
2018-01-28 10:33:17 -0800

    Fixing failing test

--- test/workbox-range-requests/node/Plugin.mjs
+++ test/workbox-range-requests/node/Plugin.mjs
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import {Plugin} from '../../../packages/workbox-range-requests/Plugin.mjs';
 
 describe(`[workbox-range-requests] Plugin`, function() {
-  it(`should expose a static cachedResponseWillBeUsed method`, function() {
-    expect(Plugin).itself.to.respondTo('cachedResponseWillBeUsed');
+  it(`should expose cachedResponseWillBeUsed method`, function() {
+    expect(Plugin.prototype).itself.to.respondTo('cachedResponseWillBeUsed');
   });
 });
