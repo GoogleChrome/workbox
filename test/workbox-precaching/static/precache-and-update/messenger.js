66824df18f7852a46ab12b21f19b525255a97fba
Jeff Posnick <jeffy@google.com>
2018-03-23 13:28:08 -0400

    Use the correct IDB database name.

--- test/workbox-precaching/static/precache-and-update/messenger.js
+++ test/workbox-precaching/static/precache-and-update/messenger.js
@@ -1,28 +1,37 @@
+// This needs to be kept in sync with the naming convention used by workbox-precaching.
+const DB_NAME = 'workbox-precache-http___localhost_3004_test_workbox-precaching_static_precache-and-update_';
+
 const messageClients = async (data) => {
-  const windowClients = await self.clients.matchAll();
-  windowClients.forEach((client) => {
+  const clients = await self.clients.matchAll();
+  for (const client of clients) {
     client.postMessage(data);
-  });
+  }
 };
 
-// This is needed by Safari as SW IDB is seperate from Window IDB
-self.addEventListener('message', (event) => {
-  const request = indexedDB.open('workbox-precaching');
-  request.onerror = async function(event) {
+// Work around the fact that Safari makes it hard to read IDB entries that were
+// set in the service worker from a window client by listening for an incoming
+// message, and messaging out the IDB entries to all clients.
+self.addEventListener('message', () => {
+  const request = indexedDB.open(DB_NAME);
+
+  request.onerror = function() {
     messageClients('Error opening indexedDB');
   };
 
   request.onsuccess = function(event) {
-    const db = event.target.result;
-
-    const transaction = db.transaction(['precached-details-models'], 'readwrite');
-    const objectStore = transaction.objectStore('precached-details-models');
-    const getAllRequest = objectStore.getAll();
-    getAllRequest.onerror = function(event) {
-      messageClients('Error opening getting all content');
-    };
-    getAllRequest.onsuccess = function(event) {
-      messageClients(event.target.result);
-    };
+    try {
+      const db = event.target.result;
+      const transaction = db.transaction('precached-details-models', 'readonly');
+      const objectStore = transaction.objectStore('precached-details-models');
+      const getAllRequest = objectStore.getAll();
+      getAllRequest.onerror = function() {
+        messageClients('Error calling getAll()');
+      };
+      getAllRequest.onsuccess = function(event) {
+        messageClients(event.target.result);
+      };
+    } catch (error) {
+      messageClients(error);
+    }
   };
 });
