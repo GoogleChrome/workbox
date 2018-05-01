7c5ab89bedff6e8ecc45904f4d6b1712180c31ad
Jeff Posnick <jeffy@google.com>
2018-05-01 13:32:08 -0400

    Fallback to network if there's a cache miss for registerNavigationRoute

--- packages/workbox-routing/_default.mjs
+++ packages/workbox-routing/_default.mjs
@@ -161,14 +161,35 @@ class DefaultRouter extends Router {
     }
 
     const cacheName = cacheNames.getPrecacheName(options.cacheName);
-    const handler = () => caches.match(cachedAssetUrl, {cacheName});
+    const handler = () => caches.match(cachedAssetUrl, {cacheName})
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        // This shouldn't normally happen, but there are edge cases:
+        // https://github.com/GoogleChrome/workbox/issues/1441
+        throw new Error(`The cache ${cacheName} did not have an entry for ` +
+          `${cachedAssetUrl}.`);
+      }).catch((error) => {
+        // If there's either a cache miss, or the caches.match() call threw
+        // an exception, then attempt to fulfill the navigation request with
+        // a response from the network rather than leaving the user with a
+        // failed navigation.
+        if (process.env.NODE_ENV !== 'production') {
+          logger.debug(`Unable to respond to navigation request with cached ` +
+            `response: ${error.message}. Falling back to network.`);
+        }
+
+        // This might still fail if the browser is offline...
+        return fetch(cachedAssetUrl);
+      });
+
     const route = new NavigationRoute(handler, {
       whitelist: options.whitelist,
       blacklist: options.blacklist,
     });
-    super.registerRoute(
-      route
-    );
+    super.registerRoute(route);
+
     return route;
   }
 }
