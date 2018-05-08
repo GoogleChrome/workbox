fc1bf6e9d2f0529501aa7581368332325f93a6c8
Jeff Posnick <jeffy@google.com>
2018-05-08 12:37:36 -0700

    Updated tests.

--- test/workbox-streams/node/test-isSupported.mjs
+++ test/workbox-streams/node/test-isSupported.mjs
@@ -3,22 +3,34 @@ import {expect} from 'chai';
 import {isSupported} from '../../../packages/workbox-streams/isSupported.mjs';
 
 describe(`[workbox-streams] isSupported`, function() {
-  const originalSelf = global.self;
+  const originalReadableStream = global.ReadableStream;
 
   afterEach(function() {
-    global.self = originalSelf;
+    global.ReadableStream = originalReadableStream;
   });
 
   it(`should return true when ReadableStream is available`, function() {
-    global.self = {
-      // This could be set to anything.
-      ReadableStream: Object.prototype,
-    };
+    class ReadableStream {
+      constructor() {
+        // no-op
+      }
+    }
+    global.ReadableStream = ReadableStream;
     expect(isSupported()).to.be.true;
   });
 
   it(`should return false when ReadableStream is not available`, function() {
-    global.self = {};
+    global.ReadableStream = undefined;
+    expect(isSupported()).to.be.false;
+  });
+
+  it(`should return false when ReadableStream throws during construction`, function() {
+    class ReadableStream {
+      constructor() {
+        throw new Error();
+      }
+    }
+    global.ReadableStream = ReadableStream;
     expect(isSupported()).to.be.false;
   });
 });
