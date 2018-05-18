90883e6b99f82708c445a029eb3800f400fe03e4
Jeffrey Posnick <jeffy@google.com>
2018-05-18 14:13:50 -0400

    Improve the code that swaps 'prod' for 'dev' in copyWorkboxLibraries (#1488)
    * (Slightly) improve the code that swaps 'prod' for 'dev' in filepaths.
    
    * Update some strings.
    
    * Typo.
    
    * String mismatch in a test.
    
    * Sigh... adding @private

--- packages/workbox-build/src/lib/use-build-type.js
+++ packages/workbox-build/src/lib/use-build-type.js
@@ -14,9 +14,36 @@
   limitations under the License.
 */
 
-// TODO (jeffposnick): More flexibility in case naming conventions change.
+// This is the build type that is expected to be present "by default".
 const DEFAULT_BUILD_TYPE = 'prod';
 
+/**
+ * Switches a string from using the "default" build type to an alternative
+ * build type. In practice, this is used to swap out "prod" for "dev" in the
+ * filename of a Workbox library.
+ *
+ * @param {string} source The path to a file, which will normally contain
+ * DEFAULT_BUILD_TYPE somewhere in it.
+ * @param {string} buildType The alternative build type value to swap in.
+ * @return {string} source, with the last occurrence of DEFAULT_BUILD_TYPE
+ * replaced with buildType.
+ * @private
+ */
 module.exports = (source, buildType) => {
-  return source.replace(DEFAULT_BUILD_TYPE, buildType);
+  // If we want the DEFAULT_BUILD_TYPE, then just return things as-is.
+  if (buildType === DEFAULT_BUILD_TYPE) {
+    return source;
+  }
+  // Otherwise, look for the last instance of DEFAULT_BUILD_TYPE, and replace it
+  // with the new buildType. This is easier via split/join than RegExp.
+  const parts = source.split(DEFAULT_BUILD_TYPE);
+  // Join the last two split parts with the new buildType. (If parts only has
+  // one item, this will be a no-op.)
+  const replaced = parts.slice(parts.length - 2).join(buildType);
+  // Take the remaining parts, if any exist, and join them with the replaced
+  // part using the DEFAULT_BUILD_TYPE, to restore any other matches as-is.
+  return [
+    ...parts.slice(0, parts.length - 2),
+    replaced,
+  ].join(DEFAULT_BUILD_TYPE);
 };
