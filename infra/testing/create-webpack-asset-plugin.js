9d6e51326d50c8fa00f8a82c7e3e8057c1aef037
Jeff Posnick <jeffy@google.com>
2018-05-23 16:38:57 -0400

    Switched over to running the webpack tests against v4.

---
+++ infra/testing/create-webpack-asset-plugin.js
@@ -0,0 +1,24 @@
+// A small helper class to generate a "fake" webpack asset for testing purposes.
+// Roughly equivalent to https://www.npmjs.com/package/generate-asset-webpack-plugin,
+// but this uses the webpack v4 syntax.
+
+class CreateWebpackAssetPlugin {
+  constructor(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Please pass in a string.');
+    }
+    this.name = name;
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tap(
+      this.constructor.name,
+      (compilation) => compilation.assets[this.name] = {
+        source: () => this.name,
+        size: () => this.name.length,
+      }
+    );
+  }
+}
+
+module.exports = CreateWebpackAssetPlugin;
