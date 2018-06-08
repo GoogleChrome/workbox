9b209339a3b8cfed275a15c53fa9f590f15dfd45
Jeff Posnick <jeffy@google.com>
2018-06-08 14:50:08 -0400

    Removed some unused testing helpers.

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,6 @@ module.exports = {
     files: [
       'packages/workbox-core/_private/logger.mjs',
       'packages/workbox-sw/_default.mjs',
-      'infra/testing/cli-test-helper.js',
       'infra/utils/log-helper.js',
       'packages/workbox-cli/src/lib/logger.js',
     ],
