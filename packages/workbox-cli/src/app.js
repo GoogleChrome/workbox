220a2cabf6013a43f833977f9332139897fd44cd
Jeff Posnick <jeffy@google.com>
2018-11-30 15:21:58 -0500

    CLI support for --watch

--- packages/workbox-cli/src/app.js
+++ packages/workbox-cli/src/app.js
@@ -10,6 +10,7 @@ const assert = require('assert');
 const ol = require('common-tags').oneLine;
 const path = require('path');
 const prettyBytes = require('pretty-bytes');
+const watch = require('glob-watcher');
 const workboxBuild = require('workbox-build');
 
 const constants = require('./lib/constants');
@@ -17,6 +18,36 @@ const errors = require('./lib/errors');
 const logger = require('./lib/logger');
 const readConfig = require('./lib/read-config');
 const runWizard = require('./lib/run-wizard');
+
+/**
+ * Runs the specified build command with the provided configuration.
+ *
+ * @param {Object} options
+ */
+async function runBuildCommand({command, config, watch}) {
+  try {
+    const {size, count, warnings} = await workboxBuild[command](config);
+
+    for (const warning of warnings) {
+      logger.warn(warning);
+    }
+
+    logger.log(`The service worker was written to ${config.swDest}\n` +
+      `${count} files will be precached, totalling ${prettyBytes(size)}.`);
+
+    if (watch) {
+      logger.log(`\nWatching for changes...`);
+    }
+  } catch (error) {
+    // See https://github.com/hapijs/joi/blob/v11.3.4/API.md#errors
+    if (typeof error.annotate === 'function') {
+      throw new Error(
+          `${errors['config-validation-failed']}\n${error.annotate()}`);
+    }
+    logger.error(errors['workbox-build-runtime-error']);
+    throw error;
+  }
+}
 
 module.exports = async (params = {}) => {
   // This should not be a user-visible error, unless meow() messes something up.
@@ -47,9 +78,9 @@ module.exports = async (params = {}) => {
 
     case 'generateSW':
     case 'injectManifest': {
-    // TODO: Confirm that this works with Windows paths.
       const configPath = path.resolve(process.cwd(),
           option || constants.defaultConfigFile);
+
       let config;
       try {
         config = readConfig(configPath);
@@ -59,23 +90,21 @@ module.exports = async (params = {}) => {
       }
 
       logger.log(`Using configuration from ${configPath}.`);
-      try {
-        const {size, count, warnings} = await workboxBuild[command](config);
 
-        for (const warning of warnings) {
-          logger.warn(warning);
+      // Determine whether we're in --watch mode, or one-off mode.
+      if (params.flags && params.flags.watch) {
+        const options = {ignoreInitial: false};
+        if (config.globIgnores) {
+          options.ignored = config.globIgnores;
+        }
+        if (config.globDirectory) {
+          options.cwd = config.globDirectory;
         }
 
-        logger.log(`The service worker was written to ${config.swDest}\n` +
-          `${count} files will be precached, totalling ${prettyBytes(size)}.`);
-      } catch (error) {
-      // See https://github.com/hapijs/joi/blob/v11.3.4/API.md#errors
-        if (typeof error.annotate === 'function') {
-          throw new Error(
-              `${errors['config-validation-failed']}\n${error.annotate()}`);
-        }
-        logger.error(errors['workbox-build-runtime-error']);
-        throw error;
+        watch(config.globPatterns, options,
+            () => runBuildCommand({command, config, watch: true}));
+      } else {
+        await runBuildCommand({command, config, watch: false});
       }
       break;
     }
