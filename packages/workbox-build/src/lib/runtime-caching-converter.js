560ac6b45fa3943d96218b0aca5d3d8583d84bc1
Jeff Posnick <jeffy@google.com>
2019-06-14 13:50:50 -0400

    Linting.

--- packages/workbox-build/src/lib/runtime-caching-converter.js
+++ packages/workbox-build/src/lib/runtime-caching-converter.js
@@ -15,6 +15,7 @@ const stringifyWithoutComments = require('./stringify-without-comments');
  * Given a set of options that configures runtime caching behavior, convert it
  * to the equivalent Workbox method calls.
  *
+ * @param {ModuleRegistry} moduleRegistry
  * @param {Object} options See
  *        https://developers.google.com/web/tools/workbox/modules/workbox-build#generateSW-runtimeCaching
  * @return {string} A JSON string representing the equivalent options.
