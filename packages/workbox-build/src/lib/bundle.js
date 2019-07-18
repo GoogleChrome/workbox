390dcac21320012edb8625271a12e8c6cbe965e2
Jeffrey Posnick <jeffy@google.com>
2019-07-18 18:11:26 -0400

    npm dependency updates (#2126)

--- packages/workbox-build/src/lib/bundle.js
+++ packages/workbox-build/src/lib/bundle.js
@@ -10,7 +10,7 @@ const {rollup} = require('rollup');
 const {terser} = require('rollup-plugin-terser');
 const {writeFile} = require('fs-extra');
 const babel = require('rollup-plugin-babel');
-const loadz0r = require('rollup-plugin-loadz0r');
+const omt = require('rollup-plugin-off-main-thread');
 const path = require('path');
 const presetEnv = require('@babel/preset-env');
 const replace = require('rollup-plugin-replace');
@@ -66,9 +66,7 @@ module.exports = async ({
   // Rollup will inline the runtime by default. If we don't want that, we need
   // to add in some additional config.
   if (!inlineWorkboxRuntime) {
-    rollupConfig.plugins.unshift(loadz0r({
-      loader: require('./loadz0r-loader'),
-    }));
+    rollupConfig.plugins.unshift(omt());
     rollupConfig.manualChunks = (id) => {
       return id.includes('workbox') ? 'workbox' : undefined;
     };
