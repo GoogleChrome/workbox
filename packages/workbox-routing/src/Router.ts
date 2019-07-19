ec3ea535279319b9e673cf20fdd74a00ae9aa42c
Jeff Posnick <jeffy@google.com>
2019-07-19 16:14:26 -0400

    Pass more matchCallback return values

--- packages/workbox-routing/src/Router.ts
+++ packages/workbox-routing/src/Router.ts
@@ -293,13 +293,15 @@ class Router {
       let params;
       let matchResult = route.match({url, request, event});
       if (matchResult) {
-        if (Array.isArray(matchResult) && matchResult.length > 0) {
+        // See https://github.com/GoogleChrome/workbox/issues/2079
+        params = matchResult;
+        if (Array.isArray(matchResult) && matchResult.length === 0) {
           // Instead of passing an empty array in as params, use undefined.
-          params = matchResult;
+          params = undefined;
         } else if ((matchResult.constructor === Object &&
-            Object.keys(matchResult).length > 0)) {
+            Object.keys(matchResult).length === 0)) {
           // Instead of passing an empty object in as params, use undefined.
-          params = matchResult;
+          params = undefined;
         }
 
         // Return early if have a match.
