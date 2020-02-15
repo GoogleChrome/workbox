28930a5dffc08cb8c562106529dffef53196421c
Jeff Posnick <jeffy@google.com>
2020-02-15 17:33:05 -0500

    Try again

--- infra/should-run-tests.js
+++ infra/should-run-tests.js
@@ -15,13 +15,5 @@ const output = stdout.toString();
 
 console.log('Files changed:', output);
 
-const matched = output.toString().split('\n')
+process.env.SHOULD_RUN_TESTS = output.toString().split('\n')
     .some((file) => file.match(/\.(?:js|json|mjs|ts|yml)$/));
-
-if (matched) {
-  console.log('At least one file matches.');
-  process.exit(0);
-}
-
-console.log('No files match.');
-process.exit(1);
