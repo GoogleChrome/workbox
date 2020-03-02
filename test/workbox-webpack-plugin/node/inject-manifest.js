a15daf147b265b053967eac0f212bf3ba329690f
Jeff Posnick <jeffy@google.com>
2020-03-02 14:59:11 -0500

    WIP

--- test/workbox-webpack-plugin/node/inject-manifest.js
+++ test/workbox-webpack-plugin/node/inject-manifest.js
@@ -1239,6 +1239,59 @@ describe(`[workbox-webpack-plugin] InjectManifest (End to End)`, function() {
       });
     });
 
+    // See https://github.com/GoogleChrome/workbox/issues/2376
+    it(`•should use webpackCompilationPlugins with DefinePlugin`, function(done) {
+      const prefix = 'replaced-by-define-plugin';
+      const swSrc = upath.join(__dirname, '..', 'static', 'sw-src-define-plugin.js');
+      const outputDir = tempy.directory();
+      const config = {
+        mode: 'production',
+        entry: upath.join(SRC_DIR, WEBPACK_ENTRY_FILENAME),
+        output: {
+          filename: '[name].[hash:20].js',
+          path: outputDir,
+        },
+        plugins: [
+          new InjectManifest({
+            swSrc,
+            swDest: 'service-worker.js',
+            webpackCompilationPlugins: [
+              new webpack.DefinePlugin({
+                __PREFIX__: prefix,
+              }),
+            ],
+          }),
+        ],
+      };
+
+      const compiler = webpack(config);
+      compiler.run(async (webpackError, stats) => {
+        const swFile = upath.join(outputDir, 'service-worker.js');
+        try {
+          webpackBuildCheck(webpackError, stats);
+
+          const files = await globby(outputDir);
+          expect(files).to.have.length(2);
+
+          await validateServiceWorkerRuntime({
+            swFile,
+            entryPoint: 'injectManifest',
+            expectedMethodCalls: {
+              setCacheNameDetails: [[{prefix}]],
+              precacheAndRoute: [[[{
+                revision: '32_CHARACTER_HASH',
+                url: 'main.20_CHARACTER_HASH.js',
+              }], {}]],
+            },
+          });
+
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+
     it(`should use manifestTransforms`, function(done) {
       const outputDir = tempy.directory();
       const warningMessage = 'test warning';
