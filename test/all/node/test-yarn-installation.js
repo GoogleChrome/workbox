9b6c7f463693866e5330e8e351abf801e2db090d
Jeff Posnick <jeffy@google.com>
2020-03-24 13:33:44 -0400

    Test yarn installation

---
+++ test/all/node/test-yarn-installation.js
@@ -0,0 +1,43 @@
+/*
+  Copyright 2020 Google LLC
+
+  Use of this source code is governed by an MIT-style
+  license that can be found in the LICENSE file or at
+  https://opensource.org/licenses/MIT.
+*/
+
+const tempy = require('tempy');
+const util = require('util');
+const childProcess = require('child_process');
+
+const exec = util.promisify(childProcess.exec);
+
+const packagesToInstall = [
+  'workbox-build',
+  'workbox-cli',
+  'workbox-webpack-plugin',
+];
+
+let temporaryDirectory;
+
+describe('[all] Yarn Installation', function() {
+  before(async function() {
+    temporaryDirectory = await tempy.directory();
+  });
+
+  for (const packageToInstall of packagesToInstall) {
+    it(`should install ${packageToInstall} using yarn`, async function() {
+      try {
+        await exec(`yarn add ${packageToInstall}`, {cwd: temporaryDirectory});
+      } catch (error) {
+        if (error.code === 127) {
+          // Skip the test if yarn isn't installed.
+          // (It will always be installed on GitHub Actions.)
+          this.skip();
+        } else {
+          throw error;
+        }
+      }
+    });
+  } 
+});
