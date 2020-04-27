31f5f160a61315f37f1fcd0f0ebfc4b4bdad08b7
philkrie <philkrie@google.com>
2020-04-27 16:58:45 -0700

    incrementing versions

--- demos/src/workbox-precaching/sw-1.js
+++ demos/src/workbox-precaching/sw-1.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 workbox.setConfig({
   debug: true,
--- demos/src/workbox-strategies/sw.js
+++ demos/src/workbox-strategies/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // Note: Ignore the error that Glitch raises about workbox being undefined.
 workbox.setConfig({
