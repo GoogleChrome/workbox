602f6408f9e4bd7da8071e21469c59d7f4cfd279
Jeff Posnick <jeffy@google.com>
2020-05-19 15:52:33 -0400

    Override dist-tag via option

--- gulp-tasks/publish-lerna.js
+++ gulp-tasks/publish-lerna.js
@@ -14,12 +14,19 @@ const logHelper = require('../infra/utils/log-helper');
 async function publish_lerna() {
   const options = ['publish', '--force-publish'];
 
-  // If we're not on master, publish to @next on npm.
-  const {stdout} = await execa('git', ['symbolic-ref', '--short', 'HEAD']);
-  if (stdout !== 'master') {
-    logHelper.log(ol`Using ${logHelper.highlight('--dist-tag=next')} as current
-        git branch is ${logHelper.highlight(stdout)}.`);
-    options.push('--dist-tag', 'next');
+  // gulp publish_lerna --distTag=blah takes precedence.
+  if (global.cliOptions.distTag) {
+    logHelper.log(ol`Using ${logHelper.highlight(
+        '--dist-tag=' + global.cliOptions.distTag)}`);
+    options.push('--dist-tag', global.cliOptions.distTag);
+  } else {
+    // If we're not on master, publish to next on npm.
+    const {stdout} = await execa('git', ['symbolic-ref', '--short', 'HEAD']);
+    if (stdout !== 'master') {
+      logHelper.log(ol`Using ${logHelper.highlight('--dist-tag=next')} as
+          the current git branch is ${logHelper.highlight(stdout)}.`);
+      options.push('--dist-tag', 'next');
+    }
   }
 
   await execa('lerna', options, {
