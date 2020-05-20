12584d52f9ff703f8a1fa552de8748bf887c6811
Jeff Posnick <jeffy@google.com>
2020-05-19 20:48:23 -0400

    Linting

--- test/all/node/test-jsdocs.js
+++ test/all/node/test-jsdocs.js
@@ -15,12 +15,12 @@ const {docs_build} = require('../../../gulp-tasks/docs.js');
 
 describe('[all] JSDocs', function() {
   it('should run JSDocs and have no unexpected results', async function() {
-    // Windows is super unhappy with the JSDocs build pipeline.	
-    // With gulp.cmd in spawn, the query string used by the baseline template	
-    // causes issues.	
-    if (process.platform === 'win32') {	
-      this.skip();	
-      return;	
+    // Windows is super unhappy with the JSDocs build pipeline.
+    // With gulp.cmd in spawn, the query string used by the baseline template
+    // causes issues.
+    if (process.platform === 'win32') {
+      this.skip();
+      return;
     }
 
     this.timeout(60 * 1000);
