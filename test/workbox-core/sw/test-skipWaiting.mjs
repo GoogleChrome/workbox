3ffee3d78f716b6d9bd33e3170bc7c3c8cfee37b
Jeffrey Posnick <jeffy@google.com>
2020-06-23 13:56:04 -0400

    Don't wrap skipWaiting (#2547)

--- test/workbox-core/sw/test-skipWaiting.mjs
+++ test/workbox-core/sw/test-skipWaiting.mjs
@@ -6,28 +6,35 @@
   https://opensource.org/licenses/MIT.
 */
 
+import {logger} from 'workbox-core/_private/logger.mjs';
 import {skipWaiting} from 'workbox-core/skipWaiting.mjs';
-
 
 describe(`skipWaiting`, function() {
   const sandbox = sinon.createSandbox();
 
-  after(function() {
+  afterEach(function() {
     sandbox.restore();
   });
 
-  it(`should add an install event listener that calls skipWaiting`, function(done) {
-    const skipWaitingSpy = sandbox.spy(self, 'skipWaiting');
-
-    sandbox.stub(self, 'addEventListener').callsFake((eventName, cb) => {
-      expect(eventName).to.equal('install');
-
-      cb();
-
-      expect(skipWaitingSpy.callCount).to.equal(1);
-      done();
-    });
+  it(`should call self.skipWaiting()`, function() {
+    const skipWaitingStub = sandbox.stub(self, 'skipWaiting');
 
     skipWaiting();
+
+    expect(skipWaitingStub.callCount).to.eql(1);
+  });
+
+  it(`should log a warning message in development`, function() {
+    if (process.env.NODE_ENV === 'production') {
+      this.skip();
+    }
+
+    const warnStub = sandbox.stub(logger, 'warn');
+    const skipWaitingStub = sandbox.stub(self, 'skipWaiting');
+
+    skipWaiting();
+
+    expect(skipWaitingStub.callCount).to.eql(1);
+    expect(warnStub.callCount).to.eql(1);
   });
 });
