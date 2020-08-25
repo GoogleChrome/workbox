03995ca446cad57ec77317b3091dc8f9d96e10ce
Jonas Wagner <jonas@29a.ch>
2020-08-25 23:40:28 +0200

    Update generate-sw.js

--- packages/workbox-build/src/generate-sw.js
+++ packages/workbox-build/src/generate-sw.js
@@ -171,7 +171,8 @@ const writeServiceWorkerUsingDefaultTemplate =
  * @param {boolean} [config.skipWaiting=false] Whether to add an
  * unconditional call to [`skipWaiting()`]{@link module:workbox-core.skipWaiting}
  * to the generated service worker. If `false`, then a `message` listener will
- * be added instead, allowing you to conditionally call `skipWaiting()`.
+ * be added instead, allowing you to conditionally call `skipWaiting()` by posting
+ * a message containing {type: 'SKIP_WAITING'}.
  *
  * @param {boolean} [config.sourcemap=true] Whether to create a sourcemap
  * for the generated service worker files.
