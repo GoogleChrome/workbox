24e8d4c4a26a067e4a1f23c77704c28dd8ddbe79
Sam Richard <snugug@google.com>
2021-01-08 15:21:17 -0500

    Add warmStrategyCache recipe
    Takes an array of items and a Workbox Strategy and warms the strategy’s cache with those items at install. Useful for getting specific items into cache but allowing them to be updated outside of the Service Worker install cycle

---
+++ packages/workbox-recipes/src/warmStrategyCache.ts
@@ -0,0 +1,30 @@
+import { Strategy } from 'workbox-strategies/src/Strategy';
+
+import './_version.js';
+
+export interface WarmStrategyCacheOptions {
+  paths: Array<string>;
+  strategy: Strategy;
+}
+
+// Give TypeScript the correct global.
+declare let self: ServiceWorkerGlobalScope;
+
+/**
+ * 
+ * @param {Object} options 
+ * @param {string[]} options.paths Paths to warm the strategy's cache with
+ * @param {Strategy} options.strategy Strategy to use
+ */
+function warmStrategyCache(options: WarmStrategyCacheOptions): void {
+  self.addEventListener('install', event => {
+    const warm = options.paths.map(path => options.strategy.handleAll({
+      event,
+      request: new Request(path),
+    })[1]);
+
+    event.waitUntil(Promise.all(warm));
+  });
+}
+
+export {warmStrategyCache};
