3151c3a2740558b87a592f5091cc795815ca3ac5
Sam Richard <snugug@google.com>
2021-01-15 14:04:10 -0500

    Feature/recipes precache and normalize (#2718)
    * Add warmStrategyCache recipe
    
    Takes an array of items and a Workbox Strategy and warms the strategy’s cache with those items at install. Useful for getting specific items into cache but allowing them to be updated outside of the Service Worker install cycle
    
    * Add warmCache and plugins options
    
    Give users of page/image/resource recipes the ability to warm those caches using warmStrategyCache functionality. Also allows them to include plugins if desired (for instance, to normalize URLs coming into the cache)
    
    * Remove offlineFallback dependency on precache
    
    While precache will still work, and that will be tried first, the files will be saved in a workbox-offline-fallbacks cache and the cache will try to be retrieved from there if precache fails.
    
    * Add missing newline
    
    * Export warmStrategyCache
    
    * Update packages/workbox-recipes/src/warmStrategyCache.ts
    
    Co-authored-by: Jeffrey Posnick <jeffy@google.com>
    
    * Rename 'warm' and 'paths'
    
    Improves clarity of each, pre PR review request
    
    * Update dependencies to use 'urls' instead of 'paths'
    
    Co-authored-by: Jeffrey Posnick <jeffy@google.com>

--- packages/workbox-recipes/src/pageCache.ts
+++ packages/workbox-recipes/src/pageCache.ts
@@ -5,10 +5,11 @@
   license that can be found in the LICENSE file or at
   https://opensource.org/licenses/MIT.
 */
+import {warmStrategyCache} from './warmStrategyCache';
 import {registerRoute} from 'workbox-routing/registerRoute.js';
 import {NetworkFirst} from 'workbox-strategies/NetworkFirst.js';
 import {CacheableResponsePlugin} from 'workbox-cacheable-response/CacheableResponsePlugin.js';
-import {RouteMatchCallback, RouteMatchCallbackOptions} from 'workbox-core/types.js';
+import {RouteMatchCallback, RouteMatchCallbackOptions, WorkboxPlugin} from 'workbox-core/types.js';
 
 import './_version.js';
 
@@ -16,6 +17,8 @@ export interface ImageCacheOptions {
   cacheName?: string;
   matchCallback?: RouteMatchCallback;
   networkTimeoutSeconds?: number;
+  plugins?: Array<WorkboxPlugin>;
+  warmCache?: Array<string>;
 }
 
 /**
@@ -27,6 +30,8 @@ export interface ImageCacheOptions {
  * @param {string} [options.cacheName] Name for cache. Defaults to pages
  * @param {RouteMatchCallback} [options.matchCallback] Workbox callback function to call to match to. Defaults to request.mode === 'navigate';
  * @param {number} [options.networkTimoutSeconds] Maximum amount of time, in seconds, to wait on the network before falling back to cache. Defaults to 3
+ * @param {WorkboxPlugin[]} [options.plugins] Additional plugins to use for this recipe
+ * @param {string[]} [options.warmCache] Paths to call to use to warm this cache
  */
 function pageCache(options: ImageCacheOptions = {}) {
   const defaultMatchCallback = ({request}: RouteMatchCallbackOptions) => request.mode === 'navigate';
@@ -34,19 +39,28 @@ function pageCache(options: ImageCacheOptions = {}) {
   const cacheName = options.cacheName || 'pages';
   const matchCallback = options.matchCallback || defaultMatchCallback;
   const networkTimeoutSeconds = options.networkTimeoutSeconds || 3;
+  const plugins = (options.plugins || []);
+  plugins.push(new CacheableResponsePlugin({
+    statuses: [0, 200],
+  }));
 
+  const strategy = new NetworkFirst({
+    networkTimeoutSeconds,
+    cacheName,
+    plugins,
+  });
+  
+
+  // Registers the route
   registerRoute(
     matchCallback,
-    new NetworkFirst({
-      networkTimeoutSeconds,
-      cacheName,
-      plugins: [
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-      ],
-    })
+    strategy
   );
+
+  // Warms the cache
+  if (options.warmCache) {
+    warmStrategyCache({urls: options.warmCache, strategy});
+  }
 }
 
 export { pageCache }
--- packages/workbox-recipes/src/staticResourceCache.ts
+++ packages/workbox-recipes/src/staticResourceCache.ts
@@ -5,16 +5,19 @@
   license that can be found in the LICENSE file or at
   https://opensource.org/licenses/MIT.
 */
+import {warmStrategyCache} from './warmStrategyCache';
 import {registerRoute} from 'workbox-routing/registerRoute.js';
 import {StaleWhileRevalidate} from 'workbox-strategies/StaleWhileRevalidate.js';
 import {CacheableResponsePlugin} from 'workbox-cacheable-response/CacheableResponsePlugin.js';
-import {RouteMatchCallback, RouteMatchCallbackOptions} from 'workbox-core/types.js';
+import {RouteMatchCallback, RouteMatchCallbackOptions, WorkboxPlugin} from 'workbox-core/types.js';
 
 import './_version.js';
 
 export interface StaticResourceOptions {
   cacheName?: string;
   matchCallback?: RouteMatchCallback;
+  plugins?: Array<WorkboxPlugin>;
+  warmCache?: Array<string>;
 }
 
 /**
@@ -24,24 +27,34 @@ export interface StaticResourceOptions {
  * 
  * @param {Object} [options]
  * @param {string} [options.cacheName] Name for cache. Defaults to static-resources
+ * @param {RouteMatchCallback} [options.matchCallback] Workbox callback function to call to match to. Defaults to request.destination === 'style' || request.destination === 'script' || request.destination === 'worker';
+ * @param {WorkboxPlugin[]} [options.plugins] Additional plugins to use for this recipe
+ * @param {string[]} [options.warmCache] Paths to call to use to warm this cache
  */
 function staticResourceCache(options: StaticResourceOptions = {}) {
   const defaultMatchCallback = ({request}: RouteMatchCallbackOptions) => request.destination === 'style' || request.destination === 'script' || request.destination === 'worker';
 
   const cacheName = options.cacheName || 'static-resources';
   const matchCallback = options.matchCallback || defaultMatchCallback;
+  const plugins = (options.plugins || []);
+  plugins.push(new CacheableResponsePlugin({
+    statuses: [0, 200],
+  }));
+
+  const strategy = new StaleWhileRevalidate({
+    cacheName,
+    plugins,
+  });
 
   registerRoute(
     matchCallback,
-    new StaleWhileRevalidate({
-      cacheName,
-      plugins: [
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-      ],
-    })
+    strategy,
   );
+
+  // Warms the cache
+  if (options.warmCache) {
+    warmStrategyCache({urls: options.warmCache, strategy});
+  }
 }
 
 export { staticResourceCache }
