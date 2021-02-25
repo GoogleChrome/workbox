43c755e76cfdb95bfa45defc354c738ca88e8ca3
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2021-02-25 19:34:32 +0100

    feat: 🎸 Add a question to workbox-cli
    Extend workbox-cli with a question regarding the
    'ignoreURLParametersMatching' workbox config parameter.
    
    ✅ Closes: #2758

---
+++ packages/workbox-cli/src/lib/questions/ask-start_url-query-params.ts
@@ -0,0 +1,53 @@
+/*
+  Copyright 2021 Google LLC
+
+  Use of this source code is governed by an MIT-style
+  license that can be found in the LICENSE file or at
+  https://opensource.org/licenses/MIT.
+*/
+
+import {prompt} from 'inquirer';
+import {oneLine as ol} from 'common-tags';
+
+const START_URL_QUERY_PARAMS_PROMPT = 'Please enter the search parameter(s) that you would like to ignore (separated by comma):';
+
+// The keys used for the questions/answers.
+const question_ignoreURLParametersMatching = 'ignoreURLParametersMatching';
+const question_shouldAskForIgnoreURLParametersMatching = 'shouldAskForIgnoreURLParametersMatching';
+
+/**
+ * @return {Promise<Object>} The answers from inquirer.
+ */
+async function askQuestion(): Promise<{ shouldAskForIgnoreURLParametersMatching: boolean; ignoreURLParametersMatching?: string }> {
+  return prompt([{
+    name: question_shouldAskForIgnoreURLParametersMatching,
+    message: ol`Does your web app manifest include search parameter(s)
+      in the 'start_url', other than 'utm_' or 'fbclid'
+      (like '?source=pwa')?`,
+    type: 'confirm',
+    default: false,
+  }, {
+    name: question_ignoreURLParametersMatching,
+    when: (answer: { shouldAskForIgnoreURLParametersMatching: boolean }) => answer.shouldAskForIgnoreURLParametersMatching,
+    message: START_URL_QUERY_PARAMS_PROMPT,
+    type: 'input',
+  }
+  ]);
+}
+
+export async function askQueryParametersInStartUrl(
+  defaultIgnoredSearchParameters: RegExp[] = [/^utm_/, /^fbclid$/]
+): Promise<RegExp[]> {
+  const { shouldAskForIgnoreURLParametersMatching, ignoreURLParametersMatching = '' } = await askQuestion();
+
+  if (!shouldAskForIgnoreURLParametersMatching) {
+    return defaultIgnoredSearchParameters;
+  }
+
+  const ignoreSearchParameters = ignoreURLParametersMatching.trim().split(',').filter(Boolean);
+
+  return defaultIgnoredSearchParameters.concat(
+    ignoreSearchParameters
+      .map(searchParam => new RegExp(`^${searchParam}`))
+  );
+}
