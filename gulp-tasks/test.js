24e04f4fd4f6c22e968b0a80891b4478c969e387
Jeffrey Posnick <jeffy@google.com>
2021-04-12 15:54:23 -0400

    Support for --skipTests (#2813)

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -11,9 +11,22 @@ const {parallel, series} = require('gulp');
 const {lint} = require('./lint');
 const {test_integration} = require('./test-integration');
 const {test_node} = require('./test-node');
+const logHelper = require('../infra/utils/log-helper');
+
+async function logSkip() {
+  logHelper.log('Skipping test suite due to --skipTests');
+}
+
+function runTestsUnlessSkipped() {
+  if (global.cliOptions.skipTests) {
+    return logSkip;
+  } else {
+    // The node and integration tests both muck with process.env.NODE_ENV,
+    // and therefore can't be run in parallel.
+    return parallel(lint, series(test_node, test_integration));
+  }
+}
 
 module.exports = {
-  // The node and integration tests both muck with process.env.NODE_ENV, and
-  // therefore can't be run in parallel.
-  test: parallel(lint, series(test_node, test_integration)),
+  test: runTestsUnlessSkipped(),
 };
