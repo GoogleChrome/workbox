7848aab20c6c5316008785b1926bf3809342635e
Adriana Jara Salazar <ajara@google.com>
2021-09-02 19:37:36 -0700

    changing durability to be relaxed

--- packages/workbox-expiration/src/models/CacheTimestampsModel.ts
+++ packages/workbox-expiration/src/models/CacheTimestampsModel.ts
@@ -111,7 +111,9 @@ class CacheTimestampsModel {
       id: this._getId(url),
     };
     const db = await this.getDb();
-    await db.put(CACHE_OBJECT_STORE, entry);
+    const tx = db.transaction(CACHE_OBJECT_STORE, 'readwrite', {durability: 'relaxed'});
+    await tx.store.put(CACHE_OBJECT_STORE, entry);
+    await tx.done;
   }
 
   /**
