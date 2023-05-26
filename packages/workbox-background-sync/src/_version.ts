252644491d9bb5a67518935ede6df530107c9475
Adriana Jara Salazar <ajara@google.com>
2023-05-26 13:27:12 -0700

    v6.6.0

--- packages/workbox-background-sync/src/_version.ts
+++ packages/workbox-background-sync/src/_version.ts
@@ -1,2 +1,2 @@
 // @ts-ignore
-try{self['workbox:background-sync:6.5.4']&&_()}catch(e){}
+try{self['workbox:background-sync:6.6.0']&&_()}catch(e){}
--- packages/workbox-strategies/src/_version.ts
+++ packages/workbox-strategies/src/_version.ts
@@ -1,2 +1,2 @@
 // @ts-ignore
-try{self['workbox:strategies:6.5.4']&&_()}catch(e){}
+try{self['workbox:strategies:6.6.0']&&_()}catch(e){}
